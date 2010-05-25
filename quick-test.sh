496f00f4e5b14be7ecbd7f1381cbba295f30d28c
Colin Ian King <colin.king@canonical.com>
2010-05-25 13:45:45 +0100

    Align quick-test.sh with new test running mechanism

--- quick-test.sh
+++ quick-test.sh
@@ -2,17 +2,5 @@
 
 LOG=results.log
 
-ARGS="--results-output=$LOG --results-no-separators --stdout-summary"
-
-tests="./src/acpi/klog/klog ./src/acpi/syntaxcheck/syntaxcheck ./src/dmi/dmi_decode/dmi_decode ./src/acpi/wakealarm/wakealarm"
-
 echo "Results being dumped into $LOG"
-rm -f $LOG
-
-for I in $tests
-do
-	echo Test: `basename $I`
-	sudo $I $ARGS
-	echo " " >> $LOG
-	echo " "
-done
+sudo ./src/testsuite --stdout-summary --show-progress --results-output=$LOG

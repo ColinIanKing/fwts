4d8c43e740d9861b7ae3bc633279c755928299fd
Colin Ian King <colin.king@canonical.com>
2010-06-08 08:30:51 +0100

    Add initial mcfg test

---
+++ src/acpi/mcfg/mcfg.c
@@ -0,0 +1,218 @@
+/*
+ * Copyright (C) 2010 Canonical
+ *
+ * Portions of this code original from the Linux-ready Firmware Developer Kit
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/io.h> 
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <limits.h>
+
+#include "fwts.h"
+
+static void *mcfg_table;
+static unsigned long mcfg_size;
+
+/* Defined in PCI Firmware Specification 3.0 */
+struct mcfg_entry {
+	unsigned int low_address;
+	unsigned int high_address;
+	unsigned short segment;
+	unsigned char start_bus;
+	unsigned char end_bus;
+	unsigned char  reserved[4];
+} __attribute__ ((packed));
+
+
+static void compare_config_space(fwts_framework *fw, int segment, int device, unsigned char *space) 
+{
+	fwts_list *lspci_output;
+	fwts_list_element *item;
+
+	char command[PATH_MAX];
+	char compare_line[1024];
+	char *lspci_cmd = "/usr/bin/lspci";
+
+	snprintf(compare_line, sizeof(compare_line), 
+		"%02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x",
+		space[0],  space[1],  space[2],  space[3],
+		space[4],  space[5],  space[6],  space[7],
+		space[8],  space[9],  space[10], space[11],
+		space[12], space[13], space[14], space[15]);
+
+	sprintf(command, "%s -vxxx -s %i:%i", lspci_cmd, segment, device);
+
+	fwts_pipe_exec(command, &lspci_output);
+	if (lspci_output == NULL) {
+		fwts_log_warning(fw, "Could not execute %s", command);
+		return;
+	}
+
+	for (item = lspci_output->head; item != NULL; item = item->next) {
+		char *line = fwts_text_list_text(item);
+
+		fwts_chop_newline(line);
+
+		if (strncmp(line, "00: ",4)==0) {
+			if (strcmp(&line[4], compare_line)) {
+				fwts_log_info(fw, "%s is read from MMCONFIG, but traditional gives :\n-%s-\n", &line[4]);
+				fwts_framework_failed(fw, "PCI config space appears to not work");
+				return;
+			} else {
+				fwts_framework_passed(fw, "PCI config space verified");
+				return;
+			}
+		}
+	}
+	fwts_text_list_free(lspci_output);
+	fwts_log_warning(fw, "Possible PCI space config space defect?");
+}
+
+static int mcfg_init(fwts_framework *fw)
+{
+	if (fwts_check_root_euid(fw))
+		return 1;
+
+	if ((mcfg_table = fwts_get_acpi_table(fw, "MCFG", &mcfg_size)) == NULL) {
+		fwts_log_warning(fw, "No MCFG ACPI table found. This table is required for PCI Express*.");
+		return 1;
+	}
+
+	return 0;
+}
+
+static int mcfg_deinit(fwts_framework *fw)
+{
+	if (mcfg_table)
+		free(mcfg_table);
+
+	return 0;
+}
+
+static char *mcfg_headline(void)
+{
+	return "MCFG PCI Express* memory mapped config space";
+}
+
+static int mcfg_test1(fwts_framework *fw)
+{
+	int fd;
+	int nr, i;
+	char *table_ptr, *table_page;
+	struct mcfg_entry *table, firstentry;
+	
+	fwts_log_info(fw,
+		"This test tries to validate the MCFG table by comparing the first\n"
+		"16 bytes in the MMIO mapped config space with the 'traditional' config\n"
+		"space of the first PCI device (root bridge). The MCFG data is only\n"
+		"trusted if it is marked reserved in the E820 table.");
+
+	mcfg_size -= 36; /* general ACPI header */
+	mcfg_size -= 8;  /* 8 bytes of padding */
+
+	if ((int)mcfg_size<0) {
+		fwts_framework_failed(fw, "Invalid MCFG ACPI table size");
+		return 1;
+	}
+	nr = mcfg_size / sizeof(struct mcfg_entry);
+
+	if (!nr) {
+		fwts_framework_failed(fw, "No MCFG ACPI table entries");
+		return 1;
+	}
+
+	if ((nr * sizeof(struct mcfg_entry)) != mcfg_size) {
+		fwts_framework_failed(fw, "MCFG table is not a multiple of record size");
+		return 1;
+	}
+
+	fwts_log_info(fw, "MCFG table found, size is %i bytes (%i entries)\n", 
+			mcfg_size, nr);
+
+	table_page = table_ptr = mcfg_table;
+
+	if (table_page==NULL) {
+		fwts_framework_failed(fw, "Invalid MCFG ACPI table");
+		return 1;
+	}
+
+	table_ptr += 36 + 8; /* 36 for the acpi header, 8 for padding */
+        table = (struct mcfg_entry *) table_ptr;
+
+	firstentry = *table;
+
+	for (i = 0; i<nr; i++) {
+		fwts_log_info(fw, "Entry address : %x\n", table->low_address);
+
+#if 0
+		if (!e820_is_reserved(table->low_address)) {
+			char buf[4095];
+			sprintf(buf, "E820: MCFG mmio config space at 0x%x is not reserved in the E820 table", table->low_address);
+			report_result("mcfg", FAIL, buf, NULL, "e820://");
+			goto out;
+		}
+#endif
+		fwts_log_info(fw, "High  address : %x \n", table->high_address);
+		fwts_log_info(fw, "Segment       : %i \n", table->segment);
+		fwts_log_info(fw, "Start bus     : %i \n", table->start_bus);
+		fwts_log_info(fw, "End bus       : %i \n", table->end_bus);
+
+		table++;
+	}
+
+	if ((fd = open("/dev/mem", O_RDONLY)) < 0) {
+		fwts_log_error(fw, "Cannot open /dev/mem");
+		return 1;
+	}
+
+	if ((table_page = mmap(NULL, getpagesize(), PROT_READ, MAP_SHARED, fd, firstentry.low_address)) == NULL) {
+		fwts_log_error(fw, "Cannot mmap onto /dev/mem");
+		return 1;
+	}
+
+        if (table_page==(void*)-1) {
+		fwts_log_error(fw, "Cannot mmap onto /dev/mem");
+                return 1;
+        }
+
+	compare_config_space(fw, firstentry.segment, 0, (unsigned char *)table_page);
+
+	munmap(table_page, getpagesize());
+	
+	return 0;
+}
+
+static fwts_framework_tests mcfg_tests[] = {
+	mcfg_test1,
+	NULL
+};
+
+static fwts_framework_ops mcfg_ops = {
+	mcfg_headline,
+	mcfg_init,
+	mcfg_deinit,
+	mcfg_tests
+};
+
+FRAMEWORK(mcfg, &mcfg_ops, TEST_ANYTIME);

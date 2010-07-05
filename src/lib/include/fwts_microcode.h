0eb5248d68628dfe16389613bb74c5d7f015c22e
Colin Ian King <colin.king@canonical.com>
2010-07-05 17:02:57 +0100

    lib: microcode - add in microcode loading

--- src/lib/include/fwts_microcode.h
+++ src/lib/include/fwts_microcode.h
@@ -17,42 +17,12 @@
  *
  */
 
-#ifndef __FWTS_H__
-#define __FWTS_H__
+#ifndef __FWTS_MICROCODE_H__
+#define  __FWTS_MICROCODE_H__
 
-#include "fwts_version.h"
+#define FWTS_MICROCODE_DEVICE	"/dev/cpu/microcode"
+#define FWTS_MICROCODE_FILE	"/usr/share/misc/intel-microcode.dat"
 
-#include "fwts_types.h"
-
-#include "fwts_binpaths.h"
-
-#include "fwts_framework.h"
-#include "fwts_log.h"
-#include "fwts_list.h"
-
-#include "fwts_text_list.h"
-
-#include "fwts_set.h"
-#include "fwts_get.h"
-
-#include "fwts_acpi.h"
-#include "fwts_acpid.h"
-#include "fwts_checkeuid.h"
-#include "fwts_cpu.h"
-#include "fwts_e820.h"
-#include "fwts_dsdt.h"
-#include "fwts_fileio.h"
-#include "fwts_gpe.h"
-#include "fwts_iasl.h"
-#include "fwts_klog.h"
-#include "fwts_pipeio.h"
-#include "fwts_stringextras.h"
-#include "fwts_wakealarm.h"
-#include "fwts_virt.h"
-#include "fwts_formatting.h"
-#include "fwts_summary.h"
-#include "fwts_microcode.h"
-
-#include "fwts_interactive.h"
+int fwts_update_microcode(fwts_framework *fw, const char *device, const char *filename);
 
 #endif

d0d10806d4fe947909a11cfef75c8b0ee6bd8bb2
Colin Ian King <colin.king@canonical.com>
2010-07-15 15:22:31 +0100

    acpi: battery: make test more thorough and make it an interactive test
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- src/acpi/battery/battery.c
+++ src/acpi/battery/battery.c
@@ -30,11 +30,25 @@
 
 #include "fwts.h"
 
+static void battery_discharge(fwts_framework *fw, const int secs)
+{
+	int i;
+	fwts_cpu_consume_start();
+
+	for (i=0;i<secs;i++) {
+		fwts_printf(fw, "Waiting %2.2d/%d\r", secs-i, secs);
+		sleep(1);
+	}
+
+	fwts_cpu_consume_complete();
+}
+
 static unsigned long get_full(char *dir)
 {
 	FILE *file;
 	char path[PATH_MAX];
 	unsigned long value = 0;
+	char buffer[4096];
 
 	if (!dir)
 		return 0;
@@ -43,21 +57,76 @@ static unsigned long get_full(char *dir)
 	if ((file = fopen(path, "r")) == NULL)
 		return 0;
 	
-	while (!feof(file)) {
-		char buffer[4096];
-		memset(buffer, 0, 4096);
-
-		if (fgets(buffer, 4095, file) == NULL)
+	while (fgets(buffer, 4095, file) != NULL) {
+		if (strstr(buffer,"remaining capacity:") && strlen(buffer)>25) {
+			value = strtoull(buffer+25, NULL, 10);	
 			break;
-		if (strstr(buffer,"remaining capacity:") && strlen(buffer)>25) 
-			value = strtoull(buffer+25, NULL, 10);
-		
+		}
 	}
 	fclose(file);
 	return value;
 }
 
-static void check_charging(fwts_framework *fw, char *dir, char *uri, char *name)
+static int wait_for_acpi_event(fwts_framework *fw, char *name)
+{
+	int gpe_count = 0;
+	int fd;
+	int events = 0;
+	int matching = 0;
+	int len;
+	char *buffer;
+	int i;
+
+	fwts_gpe *gpes_start;
+	fwts_gpe *gpes_end;
+
+	if ((gpe_count = fwts_gpe_read(&gpes_start)) == FWTS_ERROR) {
+		fwts_log_error(fw, "Cannot read GPEs.");
+		return FWTS_ERROR;
+	}
+
+	if ((fd = acpi_event_open()) < 0) {
+		fwts_log_error(fw, "Cannot connect to acpid.");
+		return FWTS_ERROR;
+	}
+
+	for (i=0;i<=20;i++) {
+		if ((buffer = acpi_event_read(fd, &len, 1)) != NULL) {
+			char *str;
+			if ((str = strstr(buffer, "battery")) != NULL) {
+				events++;
+				if (strstr(str, name) != NULL) {
+					matching++;
+					free(buffer);
+					break;
+				}
+			}
+			free(buffer);
+		}
+		fwts_printf(fw, "Waiting %2.2d/20\r", 20-i);
+	}
+	acpi_event_close(fd);
+
+	if ((gpe_count = fwts_gpe_read(&gpes_end)) == FWTS_ERROR) {
+		fwts_log_error(fw, "Cannot read GPEs.");
+	}
+
+	fwts_gpe_test(fw, gpes_start, gpes_end, gpe_count);
+	fwts_gpe_free(gpes_start, gpe_count);
+	fwts_gpe_free(gpes_end, gpe_count);
+
+	if (events == 0)
+		fwts_failed_high(fw, "Did not detect any ACPI battery events.");
+	else 
+		fwts_passed(fw, "Detected ACPI battery events.");
+		if (matching == 0) 
+			fwts_failed_high(fw, "Could not detect ACPI events for battery %s.", name);
+		else
+			fwts_passed(fw, "Detected ACPI event for battery %s.", name);
+
+	return FWTS_OK;
+}
+static void check_charging(fwts_framework *fw, char *dir, char *name)
 {
 	int i;
 	/* when we get here we KNOW the state is "charging" */
@@ -66,19 +135,22 @@ static void check_charging(fwts_framework *fw, char *dir, char *uri, char *name)
 	if (!name)
 		return;
 
+	fwts_printf(fw, "==== Waiting to see if battery charges ====\n");
+
 	initial_value = get_full(dir);
-	for (i=1; i<120; i++) {
+	for (i=0; i<=120; i++) {
 		new_value = get_full(dir);
 		if (new_value>initial_value) {
 			fwts_passed(fw, "Battery %s charge is incrementing as expected.", name);
 			return;
 		}		
+		fwts_printf(fw, "Waiting %3.3d/120\r", 120-i);
 		sleep(1);
 	}
 	fwts_failed(fw, "Battery %s claims it's charging but no charge is added", name);
 }
 
-static void check_discharging(fwts_framework *fw, char *dir, char *uri, char *name)
+static void check_discharging(fwts_framework *fw, char *dir, char *name)
 {
 	int i;
 	/* when we get here we KNOW the state is "discharging" */
@@ -87,30 +159,38 @@ static void check_discharging(fwts_framework *fw, char *dir, char *uri, char *na
 	if (!name)
 		return;
 
+	fwts_printf(fw, "==== Waiting to see if battery discharges ====\n");
+	fwts_cpu_consume_start();
+
 	initial_value = get_full(dir);
-	for (i=1; i<24; i++) {
+	for (i=0; i<=120; i++) {
 		new_value = get_full(dir);
 		if (new_value<initial_value) {
 			fwts_passed(fw, "Battery %s charge is decrementing as expected.", name);
+			fwts_cpu_consume_complete();
 			return;
 		}		
-		fwts_cpu_consume(5);
+		fwts_printf(fw, "Waiting %3.3d/120\r", 120-i);
+		sleep(1);
 	}
+	fwts_cpu_consume_complete();
 	fwts_failed(fw, "Battery %s claims it is discharging but no charge is used.", name);
 }
 
 
-static void do_battery(fwts_framework *fw, char *dir, char *name)
+static void do_battery_test(fwts_framework *fw, char *dir, char *name)
 {
 	FILE *file;
 	char path[PATH_MAX];
-	char uri[1024];
-	int present=1;
-	char *state = NULL;
-	char *model = NULL;
+	char model[1024];
+	char state[1024];
+	char buffer[4096];
 
 	if (!dir)
 		return;
+
+	*model = '\0';
+	*state = '\0';
 
 	snprintf(path, sizeof(path), "%s/state", dir);
 	if ((file = fopen(path, "r")) == NULL) {
@@ -118,16 +198,11 @@ static void do_battery(fwts_framework *fw, char *dir, char *name)
 		return;
 	}
 
-	while (!feof(file)) {
-		char buffer[4096];
-		memset(buffer, 0, 4096);
-
-		if (fgets(buffer, 4095, file) == NULL)
+	while (fgets(buffer, 4095, file) != NULL) {
+		if (strstr(buffer,"charging state:") && strlen(buffer)>25) {
+			strncpy(state, buffer+25, sizeof(state));
 			break;
-		if (strcmp(buffer,"present:                 yes") == 0)
-			present = 1;
-		if (strstr(buffer,"charging state:") && strlen(buffer)>25) 
-			state=strdup(buffer+25);
+		}
 	}
 	fclose(file);
 
@@ -137,20 +212,15 @@ static void do_battery(fwts_framework *fw, char *dir, char *name)
 		fwts_log_warning(fw, "Battery present but undersupported - no info present.");
 		return;
 	}
-	while (!feof(file)) {
-		char buffer[4096];
-		memset(buffer, 0, 4096);
-
-		if (fgets(buffer, 4095, file) == NULL)
+	while (fgets(buffer, 4095, file) != NULL) {
+		if (strstr(buffer,"model number:") && strlen(buffer)>25) {
+			strncpy(model, buffer+25, sizeof(model));
 			break;
-		if (strcmp(buffer,"present:                 yes") == 0)
-			present = 1;
-		if (strstr(buffer,"model number:") && strlen(buffer)>25) 
-			model=strdup(buffer+25);
+		}
 	}
 	fclose(file);
 
-	if ((state == NULL) || (model == NULL)) {
+	if ((*state == '\0') || (*model == '\0')) {
 		fwts_log_warning(fw, "Battery present but name or state unsupported.");
 		return;
 	}
@@ -158,16 +228,19 @@ static void do_battery(fwts_framework *fw, char *dir, char *name)
 	fwts_chop_newline(model);
 	fwts_chop_newline(state);
 
-	fwts_log_info(fw, "Battery %s is model %s and is currently %s.", name, model, state);
+	fwts_log_info(fw, "Battery %s model %s is currently %s.", name, model, state);
 
-	if (strstr(state,"discharging"))
-		check_discharging(fw, dir, uri, name);
-	else if (strstr(state,"charging"))
-		check_charging(fw, dir, uri, name);
+	fwts_printf(fw, "==== Please PLUG IN the AC power of the machine ====\n");
+	fwts_press_enter(fw);
 
-	free(state);
-	free(model);
-	
+	fwts_printf(fw, "==== Please now UNPLUG the AC power of the machine ====\n");
+	wait_for_acpi_event(fw, name);
+	check_discharging(fw, dir, name);
+	fwts_printf(fw, "==== Please wait 30 seconds while battery is discharged a little ====\n");
+	battery_discharge(fw, 30);
+	fwts_printf(fw, "==== Please now PLUG IN the AC power of the machine ====\n");
+	wait_for_acpi_event(fw, name);
+	check_charging(fw, dir, name);
 }
 
 static char *battery_headline(void)
@@ -186,7 +259,7 @@ static int battery_test1(fwts_framework *fw)
 	   "In addition, for charging or discharging batteries, the test validates "
 	   "that the reported 'current capacity' properly increments/decrements "
 	   "in line with the charge/discharge state. "
-	   "This test also stresses the entire battery state reporting codepath "
+	   "This test also stresses the battery state reporting codepath "
 	   "in the ACPI BIOS, and any warnings given by the ACPI interpreter "
 	   "will be reported.");
 
@@ -201,7 +274,7 @@ static int battery_test1(fwts_framework *fw)
 			char batpath[2048];
 
 			snprintf(batpath, sizeof(batpath), "/proc/acpi/battery/%s", entry->d_name);
-			do_battery(fw, batpath, entry->d_name);
+			do_battery_test(fw, batpath, entry->d_name);
 			battdir++;
 		}
 	} while (entry);
@@ -226,4 +299,4 @@ static fwts_framework_ops battery_ops = {
 	battery_tests
 };
 
-FWTS_REGISTER(battery, &battery_ops, FWTS_TEST_ANYTIME, FWTS_BATCH);
+FWTS_REGISTER(battery, &battery_ops, FWTS_TEST_ANYTIME, FWTS_INTERACTIVE);

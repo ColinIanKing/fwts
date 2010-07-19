411d62852983b3075e07f4b02bf0d38199cbc88d
Colin Ian King <colin.king@canonical.com>
2010-07-19 07:40:31 +0100

    cpu: nx: check NX flag across all CPUs
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- src/cpu/nx/nx.c
+++ src/cpu/nx/nx.c
@@ -23,26 +23,6 @@
 
 #include "fwts.h"
 
-static fwts_cpuinfo_x86 *fwts_nx_cpuinfo;
-
-static int nx_init(fwts_framework *fw)
-{
-	if ((fwts_nx_cpuinfo = fwts_cpu_get_info()) == NULL) {
-		fwts_log_error(fw, "Cannot get CPU info");
-		return FWTS_ERROR;
-	}
-
-	return FWTS_OK;
-}
-
-static int nx_deinit(fwts_framework *fw)
-{
-	if (fwts_nx_cpuinfo)
-		fwts_cpu_free_info(fwts_nx_cpuinfo);
-
-	return FWTS_OK;
-}
-
 static char *nx_headline(void)
 {
 	return "Test if CPU NX is disabled by the BIOS.";
@@ -50,19 +30,29 @@ static char *nx_headline(void)
 
 static int nx_test1(fwts_framework *fw)
 {
+	fwts_cpuinfo_x86 *fwts_nx_cpuinfo;
+
+	if ((fwts_nx_cpuinfo = fwts_cpu_get_info(0)) == NULL) {
+		fwts_log_error(fw, "Cannot get CPU info");
+		return FWTS_ERROR;
+	}
+
 	if (strstr(fwts_nx_cpuinfo->flags," nx")) {
 		fwts_passed(fw, "CPU has NX flags, BIOS is not disabling it.");
+		fwts_cpu_free_info(fwts_nx_cpuinfo);
 		return FWTS_OK;
 	}
 	
 	if (strstr(fwts_nx_cpuinfo->flags," pae") == NULL) {
 		fwts_passed(fw, "CPU is not PAE capable, so it does not have NX.");
+		fwts_cpu_free_info(fwts_nx_cpuinfo);
 		return FWTS_OK;
 	}
 
 	if ((fwts_nx_cpuinfo->x86 == -1) || 
 	    (fwts_nx_cpuinfo->x86_model == -1)) {
 		fwts_failed_high(fw, "No model or family found for this CPU. Please check /proc/cpuinfo.");
+		fwts_cpu_free_info(fwts_nx_cpuinfo);
 		return FWTS_OK;
 	}
 
@@ -82,18 +72,69 @@ static int nx_test1(fwts_framework *fw)
 			"The CPU is family %d, model %s and does not have NX capabilities.",
 			fwts_nx_cpuinfo->x86, fwts_nx_cpuinfo->x86_model);
 
+	fwts_cpu_free_info(fwts_nx_cpuinfo);
+	return FWTS_OK;
+}
+
+static int nx_test2(fwts_framework *fw)
+{
+	fwts_cpuinfo_x86 *fwts_nx_cpuinfo;
+	int i;
+	int n;
+	int cpu0_has_nx;
+	int failed = 0;
+
+	fwts_log_info(fw, 
+		"This test verifies that all CPUs have the same NX flag setting. "
+		"Although rare, BIOS may set the NX flag differently "
+		"per CPU. ");
+
+	if ((n = fwts_cpu_enumerate()) == FWTS_ERROR) {
+		fwts_log_error(fw, "Cannot determine number of CPUs");
+		return FWTS_ERROR;
+	}
+
+	if (n == 1) {
+		fwts_log_info(fw, "Only one CPU, no need to run test.");
+		return FWTS_OK;
+	}
+
+	if ((fwts_nx_cpuinfo = fwts_cpu_get_info(0)) == NULL) {
+		fwts_log_error(fw, "Cannot get CPU info");
+		return FWTS_ERROR;
+	}
+	cpu0_has_nx = (strstr(fwts_nx_cpuinfo->flags," nx") != NULL);
+	fwts_cpu_free_info(fwts_nx_cpuinfo);
+
+	for (i=1; i<n; i++) {
+		if ((fwts_nx_cpuinfo = fwts_cpu_get_info(0)) == NULL) {
+			fwts_failed(fw, "Cannot get CPU%d info", i);
+			fwts_cpu_free_info(fwts_nx_cpuinfo);
+			return FWTS_ERROR;
+		}
+		if (cpu0_has_nx != (strstr(fwts_nx_cpuinfo->flags," nx") != NULL)) {
+			fwts_failed(fw, "CPU%d has different NX flags to CPU0.");
+			failed++;
+		}
+		fwts_cpu_free_info(fwts_nx_cpuinfo);
+	}
+
+	if (!failed) 
+		fwts_passed(fw, "All %d CPUs have the same NX flag.", n);
+
 	return FWTS_OK;
 }
 
 static fwts_framework_tests nx_tests[] = {
 	nx_test1,
+	nx_test2,
 	NULL
 };
 
 static fwts_framework_ops nx_ops = {
 	nx_headline,
-	nx_init,
-	nx_deinit,
+	NULL,
+	NULL,
 	nx_tests
 };
  

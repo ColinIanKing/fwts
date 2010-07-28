a67c9b3c1dbce40c70f7130cf705a82fd616482c
Colin Ian King <colin.king@canonical.com>
2010-07-28 12:05:07 +0100

    lib: fileio: chop off \n from end of lines
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- src/lib/src/fileio.c
+++ src/lib/src/fileio.c
@@ -34,7 +34,7 @@ fwts_list *fwts_file_read(FILE *fp)
 
 	while (fgets(buffer, sizeof(buffer), fp) != NULL) {
 		int len = strlen(buffer);
-		buffer[len] = '\0';	/* Chop off "\n" */
+		buffer[len-1] = '\0';	/* Chop off "\n" */
 		fwts_text_list_append(list, buffer);
 	}
 

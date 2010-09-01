08ce7288c514d4650253ccd38406e20315d5c794
Colin Ian King <colin.king@canonical.com>
2010-09-01 10:58:41 +0100

    fwts_acpi.h: correctly implement FWTS_GET_UINT64 and FWTS_GET_GAS
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- src/lib/include/fwts_acpi.h
+++ src/lib/include/fwts_acpi.h
@@ -26,36 +26,41 @@
 #include "fwts_framework.h"
 #include "fwts_log.h"
 
-#define FWTS_GET_UINT64(var, buffer, offset) \
-	var = 				\
-        ( ((uint64)data[offset+3] << 56) |      \
-          ((uint64)data[offset+2] << 48) |      \
-          ((uint64)data[offset+1] << 40) |      \
+#define FWTS_GET_UINT64(var, buffer, offset) 	\
+	var = 					\
+        ( ((uint64)data[offset+7] << 56) |      \
+          ((uint64)data[offset+6] << 48) |      \
+          ((uint64)data[offset+5] << 40) |      \
           ((uint64)data[offset+4] << 32) |      \
           ((uint64)data[offset+3] << 24) |      \
           ((uint64)data[offset+2] << 16) |      \
           ((uint64)data[offset+1] << 8)  |      \
           ((uint64)data[offset]) )
 
-#define FWTS_GET_UINT32(var, buffer, offset) \
-	var = 				\
-        ( (data[offset+3] << 24) |      \
-          (data[offset+2] << 16) |      \
-          (data[offset+1] << 8)  |      \
+#define FWTS_GET_UINT32(var, buffer, offset) 	\
+	var = 					\
+        ( (data[offset+3] << 24) |      	\
+          (data[offset+2] << 16) |      	\
+          (data[offset+1] << 8)  |      	\
           (data[offset]) )
 
-#define FWTS_GET_UINT16(var, buffer, offset) \
-	var =				\
+#define FWTS_GET_UINT16(var, buffer, offset) 	\
+	var =					\
         ( ((uint16)data[offset+1] << 8)  |      \
           ((uint16)data[offset]) )
 
 #define FWTS_GET_UINT8(var, buffer, offset)	\
-	var = 				\
+	var = 					\
         ( ((uint8)data[offset]) )
 
-/* FIX ME - this needs implementing */
-#define FWTS_GET_GAS(var, buffer, offset)
-
+#define FWTS_GET_GAS(var, buffer, offset)					\
+	do { 									\
+		FWTS_GET_UINT8(var.address_space_id, buffer, offset);		\
+		FWTS_GET_UINT8(var.register_bit_width, buffer, (offset+1));	\
+		FWTS_GET_UINT8(var.register_bit_offset, buffer, (offset+2));	\
+		FWTS_GET_UINT8(var.access_width, buffer, (offset+3));		\
+		FWTS_GET_UINT64(var.address, buffer, (offset+4));		\
+	} while (0)
 
 typedef struct {
 	uint8 	address_space_id;
@@ -76,6 +81,55 @@ typedef struct {
 	char		creator_id[4];
 	uint32		creator_revision;
 } fwts_acpi_table_header;
+
+typedef struct {
+	uint8		type;
+	uint8		length;
+} fwts_acpi_sub_table_header;
+
+typedef struct {
+	fwts_acpi_table_header	header;
+	uint32		lapic_address;
+	uint32		flags;
+} fwts_acpi_table_madt;
+
+typedef enum {
+        FWTS_ACPI_MADT_LOCAL_APIC = 0,
+        FWTS_ACPI_MADT_IO_APIC,
+        FWTS_ACPI_MADT_INTERRUPT_OVERRIDE,
+        FWTS_ACPI_MADT_NMI_SOURCE,
+        FWTS_ACPI_MADT_LOCAL_APIC_NMI,
+        FWTS_ACPI_MADT_LOCAL_APIC_OVERRIDE,
+        FWTS_ACPI_MADT_IO_SAPIC,
+        FWTS_ACPI_MADT_LOCAL_SAPIC,
+        FWTS_ACPI_MADT_INTERRUPT_SOURCE,
+        FWTS_ACPI_MADT_LOCAL_X2APIC,
+        FWTS_ACPI_MADT_LOCAL_X2APIC_NMI,
+        FWTS_ACPI_MADT_RESERVED
+} fwts_acpi_madt_type;
+
+typedef struct {
+	fwts_acpi_sub_table_header	header;
+	uint8		processor_id;
+	uint8		lapic_id;
+	uint32		lapic_flags;
+} fwts_acpi_madt_local_apic;
+
+typedef struct {
+	fwts_acpi_sub_table_header	header;
+	uint8		io_apic_id;
+	uint8		reserved;
+	uint32		apic_phys_address;
+	uint32		global_irq_base;
+} fwts_acpi_madt_io_apic;
+
+typedef struct {
+	fwts_acpi_sub_table_header	header;
+	uint8		bus;
+	uint8		source_irq;
+	uint32		global_irq;
+	uint16		int_flags;
+} fwts_acpi_madt_interrupt_override;
 
 uint8 *fwts_acpi_table_load(fwts_framework *fw, const char *name, const int which, int *size);
 

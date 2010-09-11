24933e24220f1cc0e5cb27f74d59901273d441a8
Colin Ian King <colin.king@canonical.com>
2010-09-11 14:36:29 +0100

    bios: dump ROMs
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

---
+++ src/bios/romdump/romdump.c
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2010 Canonical
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "fwts.h"
+
+#define BIOS_ROM_REGION_START	(0x000c0000)
+#define BIOS_ROM_REGION_END  	(0x000fffff)
+#define BIOS_ROM_REGION_SIZE	(BIOS_ROM_REGION_END - BIOS_ROM_REGION_START)
+
+#define BIOS_ROM_START		(0x000f0000)
+#define BIOS_ROM_END		(0x000fffff)
+#define BIOS_ROM_SIZE		(BIOS_ROM_END - BIOS_ROM_START)
+#define BIOS_ROM_OFFSET		(BIOS_ROM_START - BIOS_ROM_REGION_START)
+
+static int romdump_init(fwts_framework *fw)
+{
+	if (fwts_check_root_euid(fw))
+		return FWTS_ERROR;
+
+	return FWTS_OK;
+}
+
+static char *romdump_headline(void)
+{
+	return "Dump ROMs.";
+}
+
+static void romdump_data(fwts_framework *fw, uint8 *data, int offset, int length)
+{
+	char buffer[128];
+	int i;
+
+	for (i=0; i<length; i+=16) {
+		fwts_dump_raw_data(buffer, sizeof(buffer), data+i, offset+i, 16);
+		fwts_log_info_verbatum(fw, "%s", buffer);
+	}
+}
+
+static int romdump_test1(fwts_framework *fw)
+{
+	int fd;
+	uint8 *mem;
+	int i;
+
+	if ((fd = open("/dev/mem", O_RDONLY)) < 0) {
+		fwts_log_error(fw, "Cannot open /dev/mem.");
+		return FWTS_ERROR;
+	}
+
+        if ((mem = mmap(NULL, BIOS_ROM_REGION_SIZE, PROT_READ, MAP_PRIVATE, fd, BIOS_ROM_REGION_START)) == MAP_FAILED) {
+		fwts_log_error(fw, "Cannot mmap /dev/mem.");
+		return FWTS_ERROR;
+	}
+        close(fd);
+
+	for (i=0; i<BIOS_ROM_REGION_SIZE; i+= 512) {
+		if ((*(mem+i) == 0x55) && (*(mem+i+1) == 0xaa)) {
+			int length = *(mem+i+2) << 9;
+		
+			fwts_log_info(fw, "Found ROM: %lx..%lx (%d bytes)", BIOS_ROM_REGION_START+i, BIOS_ROM_REGION_START+i+length, length);
+			romdump_data(fw, mem+i, BIOS_ROM_REGION_START+i, length);
+			fwts_log_nl(fw);
+		}
+	}
+
+	fwts_log_info(fw, "BIOS ROM: %lx..%lx (%d bytes)", BIOS_ROM_START, BIOS_ROM_END, BIOS_ROM_SIZE);
+	
+	romdump_data(fw, mem+BIOS_ROM_OFFSET, BIOS_ROM_START, BIOS_ROM_SIZE);
+
+        munmap(mem, BIOS_ROM_REGION_SIZE);
+
+	return FWTS_OK;
+}
+
+static fwts_framework_tests romdump_tests[] = {
+	romdump_test1,
+	NULL
+};
+
+static fwts_framework_ops romdump_ops = {
+	romdump_headline,
+	romdump_init,
+	NULL,
+	romdump_tests
+};
+
+FWTS_REGISTER(romdump, &romdump_ops, FWTS_TEST_ANYTIME, FWTS_BATCH_EXPERIMENTAL);

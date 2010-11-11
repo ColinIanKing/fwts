4781cd2ac89f878e782b764bb8c15f629eb747d2
Colin Ian King <colin.king@canonical.com>
2010-11-11 15:01:58 +0000

    lib: fwts_version.h - update to V0.19.17
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- src/lib/include/fwts_version.h
+++ src/lib/include/fwts_version.h
@@ -16,5 +16,5 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  */
-#define FWTS_VERSION "V0.19.16"
-#define FWTS_DATE    "Wed Nov 10 16:25:04 GMT 2010"
+#define FWTS_VERSION "V0.19.17"
+#define FWTS_DATE    "Thu Nov 11 15:01:58 GMT 2010"

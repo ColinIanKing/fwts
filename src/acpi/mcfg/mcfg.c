383093d74edaa1de18cfa45f5cb9292cab5fd615
Colin Ian King <colin.king@canonical.com>
2010-11-25 16:19:58 +0000

    acpi: mcfg: fix missing arg of log error message.
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- src/acpi/mcfg/mcfg.c
+++ src/acpi/mcfg/mcfg.c
@@ -76,7 +76,7 @@ static void compare_config_space(fwts_framework *fw, int segment, int device, un
 
 		if (strncmp(line, "00: ",4)==0) {
 			if (strcmp(&line[4], compare_line)) {
-				fwts_log_info(fw, "%s is read from MMCONFIG, but traditional gives :\n-%s-\n", &line[4]);
+				fwts_log_info(fw, "%s is read from MMCONFIG, but traditional gives :\n-%s-\n", &line[4], compare_line);
 				fwts_failed(fw, "PCI config space appears to not work");
 			} else
 				fwts_passed(fw, "PCI config space verified");

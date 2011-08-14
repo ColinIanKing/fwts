d0bad3aa1d43c534a5dc5b936ec93dda4e735362
Colin Ian King <colin.king@canonical.com>
2011-08-14 19:54:18 +0100

    pci: crs: tidy up source.
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- src/pci/crs/crs.c
+++ src/pci/crs/crs.c
@@ -36,7 +36,8 @@ static int crs_get_bios_date(fwts_framework *fw, int *day, int *mon, int *year)
 	}
 
 	if (dmi_text == NULL) {
-		fwts_log_error(fw, "Failed to read output from dmidecode (out of memory).");
+		fwts_log_error(fw,
+			"Failed to read output from dmidecode (out of memory).");
 		return FWTS_ERROR;
 	}
 
@@ -51,7 +52,7 @@ static int crs_get_bios_date(fwts_framework *fw, int *day, int *mon, int *year)
 		}
 	}
 	fwts_list_free(dmi_text, free);
-	
+
 	return FWTS_OK;
 }
 
@@ -67,7 +68,7 @@ static int crs_test1(fwts_framework *fw)
 	}
 
 	if (crs_get_bios_date(fw, &day, &mon, &year) != FWTS_OK) {
-		fwts_log_error(fw, "Cannot determine age of BIOS.");		
+		fwts_log_error(fw, "Cannot determine age of BIOS.");
 		return FWTS_ERROR;
 	}
 
@@ -76,57 +77,65 @@ static int crs_test1(fwts_framework *fw)
 		return FWTS_ERROR;
 	}
 
-        if (fwts_klog_regex_find(fw, klog, "PCI: Ignoring host bridge windows from ACPI;") > 0) {
+        if (fwts_klog_regex_find(fw, klog,
+		"PCI: Ignoring host bridge windows from ACPI;") > 0) {
 		if (strstr(cmdline, "pci=nocrs") != NULL) {
 			fwts_skipped(fw, "Kernel was booted with pci=nocrs, Ignoring host bridge windows _CRS settings from ACPI, skipping test.");
 		}
 		else {
 			if (year == 0) {
-				fwts_failed(fw, LOG_LEVEL_MEDIUM, "BIOSTooOld",
-						"The kernel could not determine the BIOS age "
-						"and has assumed that your BIOS is too old to correctly "
-						"specify the host bridge MMIO aperture using _CRS.");
+				fwts_failed(fw, LOG_LEVEL_MEDIUM,
+					"BIOSTooOld",
+					"The kernel could not determine the BIOS age "
+					"and has assumed that your BIOS is too old to correctly "
+					"specify the host bridge MMIO aperture using _CRS.");
 				fwts_tag_failed(fw, FWTS_TAG_BIOS);
 				fwts_log_advice(fw, "You can override this by booting with \"pci=use_crs\".");
-			
+
 			} else if (year < 2008) {
-				fwts_passed(fw, "The kernel has detected an old BIOS (%d/%d/%d) "
-					    	"and has assumed that your BIOS is too old to correctly "
-					    	"specify the host bridge MMIO aperture using _CRS.", mon, day, year);
+				fwts_passed(fw,
+					"The kernel has detected an old BIOS (%d/%d/%d) "
+					"and has assumed that your BIOS is too old to correctly "
+					"specify the host bridge MMIO aperture using _CRS.", mon, day, year);
 				fwts_tag_failed(fw, FWTS_TAG_BIOS);
 				fwts_log_advice(fw, "You can override this by booting with \"pci=use_crs\".");
 			} else {
-				fwts_failed(fw, LOG_LEVEL_MEDIUM, "HostBridgeWindows",
-						"The kernel is ignoring host bridge windows from ACPI for some unknown reason. "
-						"pci=nocrs has not been used as a boot parameter and the BIOS may be recent enough "
-						"to support this (%d/%d/%d)", mon, day, year);
+				fwts_failed(fw, LOG_LEVEL_MEDIUM,
+					"HostBridgeWindows",
+					"The kernel is ignoring host bridge windows from ACPI for some unknown reason. "
+					"pci=nocrs has not been used as a boot parameter and the BIOS may be recent enough "
+					"to support this (%d/%d/%d)", mon, day, year);
 				fwts_tag_failed(fw, FWTS_TAG_BIOS);
 			}
 		}
 	} else if (fwts_klog_regex_find(fw, klog, "PCI: Using host bridge windows from ACPI;") > 0) {
 		if (strstr(cmdline, "pci=use_crs") != NULL) {
 			if (year == 0)  {
-				fwts_failed(fw, LOG_LEVEL_MEDIUM, "BIOSNoReleaseDate",
+				fwts_failed(fw, LOG_LEVEL_MEDIUM,
+					"BIOSNoReleaseDate",
 					"The BIOS does not seem to have release date, hence pci=use_crs was required.");
 				fwts_tag_failed(fw, FWTS_TAG_BIOS);
 			} else if (year < 2008) {
-				fwts_passed(fw, "The BIOS is relatively old (%d/%d/%d) and hence pci=use_crs was required to "
-						"enable host bridge windows _CRS settings from ACPI.", mon, day, year);
+				fwts_passed(fw,
+					"The BIOS is relatively old (%d/%d/%d) and hence pci=use_crs was required to "
+					"enable host bridge windows _CRS settings from ACPI.", mon, day, year);
 				fwts_tag_failed(fw, FWTS_TAG_BIOS);
 			} else {
-				fwts_failed(fw, LOG_LEVEL_LOW, "BIOSSupportBridgeWindows",
-						"Kernel was booted with pci=use_crs but this may be uncessary as "
-						"the BIOS is new enough to support automatic bridge windows configuring using _CRS from ACPI. "
-						"However, the workaround may be necessary because _CRS is incorrect or not implemented in the "
-						"DSDT.");
+				fwts_failed(fw, LOG_LEVEL_LOW,
+					"BIOSSupportBridgeWindows",
+					"Kernel was booted with pci=use_crs but this may be uncessary as "
+					"the BIOS is new enough to support automatic bridge windows configuring using _CRS from ACPI. "
+					"However, the workaround may be necessary because _CRS is incorrect or not implemented in the "
+					"DSDT.");
 				fwts_tag_failed(fw, FWTS_TAG_BIOS);
 			}
 		}
 		else {
-			fwts_passed(fw, "The kernel has detected a BIOS newer than the end of 2007 (%d/%d/%d) "
-				    "and has assumed that your BIOS can correctly "
-				    "specify the host bridge MMIO aperture using _CRS.  If this does not work "
-				    "correctly you can override this by booting with \"pci=nocrs\".", mon, day, year);
+			fwts_passed(fw,
+				"The kernel has detected a BIOS newer than the end of 2007 (%d/%d/%d) "
+				"and has assumed that your BIOS can correctly "
+				"specify the host bridge MMIO aperture using _CRS.  If this does not work "
+				"correctly you can override this by booting with \"pci=nocrs\".", mon, day, year);
 		}
 	} else {
 		fwts_skipped(fw, "Cannot find host bridge message in kernel log, skipping test.");

cc34f7e2635613d6f10dd8024e61dae6fad0633d
Colin Ian King <colin.king@canonical.com>
2011-08-14 20:42:59 +0100

    dmi: dmi_decode: fix failure format string (LP: #826418)
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- src/dmi/dmi_decode/dmi_decode.c
+++ src/dmi/dmi_decode/dmi_decode.c
@@ -33,12 +33,13 @@
 
 typedef struct {
 	fwts_log_level level;
-	char *pat1;
-	char *pat2;
-	char *message;
+	const char *label;
+	const char *pat1;
+	const char *pat2;
+	const char *message;
 } dmi_pattern;
 
-static char *dmi_types[] = {
+static const char *dmi_types[] = {
 	"BIOS",
 	"System",
 	"Base Board",
@@ -81,18 +82,82 @@ static char *dmi_types[] = {
 	"Power Supply"
 };
 
-static dmi_pattern dmi_patterns[] = {
-	{ LOG_LEVEL_HIGH,    "No SMBIOS nor DMI entry point found", NULL, "Check SMBIOS or DMI entry points" },
-	{ LOG_LEVEL_HIGH,    "Wrong DMI structures count", NULL, "DMI structures count" },
-	{ LOG_LEVEL_HIGH,    "Wrong DMI structures length",NULL, "DMI structures length" },
-	{ LOG_LEVEL_MEDIUM,  "<OUT OF SPEC>", NULL, "Out of spec check" },
-	{ LOG_LEVEL_MEDIUM,  "<BAD INDEX>", NULL, "Bad index check" },
-	{ LOG_LEVEL_HIGH,    "Bad checksum! Please report.", "Bad checksum" },
-	{ LOG_LEVEL_LOW,     "Serial Number:", "0123456789", "Template Serial Number not updated" },
-	{ LOG_LEVEL_LOW,     "Asset Tag",  "1234567890", "Template Serial Number not updated" },
-	{ LOG_LEVEL_LOW,     "UUID:", "0A0A0A0A-0A0A-0A0A-0A0A-0A0A0A0A0A0A.", "UUID number not updated" },
-	{ LOG_LEVEL_LOW,     "To Be Filled By O.E.M.", NULL, "Value not updated" },
-	{ 0, NULL, NULL, NULL }
+static const dmi_pattern dmi_patterns[] = {
+	{ 
+		LOG_LEVEL_HIGH,
+		"NoSMBIOSorDMIentry",	
+		"No SMBIOS nor DMI entry point found",
+		NULL,
+		"Check SMBIOS or DMI entry points" 
+	},
+	{ 
+		LOG_LEVEL_HIGH,
+		"DMIBadStructCount",
+		"Wrong DMI structures count",
+		NULL,
+		"DMI structures count"
+	},
+	{
+		LOG_LEVEL_HIGH,
+		"DMIBadStructLength",
+		"Wrong DMI structures length",
+		NULL,
+		"DMI structures length"
+	},
+	{
+		LOG_LEVEL_MEDIUM,
+		"DMIOutOfSpec",
+		"<OUT OF SPEC>",
+		NULL,
+		"Out of spec check"
+	},
+	{
+		LOG_LEVEL_MEDIUM,
+		"DMIBadIndex",
+		"<BAD INDEX>",
+		NULL,
+		"Bad index check"
+	},
+	{
+		LOG_LEVEL_HIGH,
+		"DMIChecksum",
+		"Bad checksum! Please report.",
+		NULL,
+		"Bad checksum"
+	},
+	{
+		LOG_LEVEL_LOW,
+		"DMISerialNumber",
+		"Serial Number:",
+		"0123456789",
+		"Template Serial Number not updated"
+	},
+	{
+		LOG_LEVEL_LOW,
+		"DMIAssetTag",
+		"Asset Tag",
+		"1234567890",
+		"Template Serial Number not updated"
+	},
+	{
+		LOG_LEVEL_LOW,
+		"DMIUUID",
+		"UUID:",
+		"0A0A0A0A-0A0A-0A0A-0A0A-0A0A0A0A0A0A.",
+		"UUID number not updated"
+	},
+	{
+		LOG_LEVEL_LOW,
+		"DMIBadDefault",
+		"To Be Filled By O.E.M.",
+		NULL,
+		"Value not updated"
+	},
+	{ 	0,
+		NULL,
+		NULL,
+		NULL
+	}
 };
 
 static int dmi_decode_init(fwts_framework *fw)
@@ -113,7 +178,8 @@ static int dmi_decode_test1(fwts_framework *fw)
 		int dumped = 0;
 		char buffer[PATH_MAX];
 
-		snprintf(buffer, sizeof(buffer), "%s -t %d", fw->dmidecode, type);
+		snprintf(buffer, sizeof(buffer), "%s -t %d",
+			fw->dmidecode, type);
 
 		if (fwts_pipe_exec(buffer, &dmi_text)) {
 			fwts_log_error(fw, "Failed to execute dmidecode.");
@@ -137,13 +203,19 @@ static int dmi_decode_test1(fwts_framework *fw)
 						(strstr(text, dmi_patterns[i].pat2) != NULL);
 				}
 				if (match) {		
-					fwts_failed(fw, dmi_patterns[i].level, "DMI type %s: %s.", dmi_types[type],dmi_patterns[i].message);
+					fwts_failed(fw,
+						dmi_patterns[i].level,
+						dmi_patterns[i].label,
+						"DMI type %s: %s.",
+						dmi_types[type],
+						dmi_patterns[i].message);
 					fwts_tag_failed(fw, FWTS_TAG_BIOS_DMI);
 					if (!dumped) {
 						fwts_log_info(fw, "DMI table dump:");
 						fwts_list_link *dump;
 						fwts_list_foreach(dump, dmi_text)
-							fwts_log_info_verbatum(fw, "%s", fwts_text_list_text(dump));			
+							fwts_log_info_verbatum(fw,
+								"%s", fwts_text_list_text(dump));			
 						dumped = 1;
 					}
 				}

c7bd4f9d4bf1f902bc1fe8bf739c62aac6a27ad1
Colin Ian King <colin.king@canonical.com>
2011-08-15 11:45:18 +0100

    acpi: mcfg: tidy up source.
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- src/acpi/mcfg/mcfg.c
+++ src/acpi/mcfg/mcfg.c
@@ -55,7 +55,8 @@ static void compare_config_space(fwts_framework *fw, int segment, int device, un
 		space[8],  space[9],  space[10], space[11],
 		space[12], space[13], space[14], space[15]);
 
-	snprintf(command, sizeof(command), "%s -vxxx -s %i:%i", fw->lspci, segment, device);
+	snprintf(command, sizeof(command), "%s -vxxx -s %i:%i",
+		fw->lspci, segment, device);
 
 	if (fwts_pipe_exec(command, &lspci_output) == FWTS_EXEC_ERROR) {
 		fwts_log_warning(fw, "Could not execute %s", command);
@@ -70,7 +71,9 @@ static void compare_config_space(fwts_framework *fw, int segment, int device, un
 		if (strncmp(line, "00: ",4)==0) {
 			if (strcmp(&line[4], compare_line)) {
 				fwts_log_info(fw, "%s is read from MMCONFIG, but traditional gives :\n-%s-\n", &line[4], compare_line);
-				fwts_failed(fw, LOG_LEVEL_MEDIUM, "PCIConfigSpaceBad", "PCI config space appears to not work");
+				fwts_failed(fw, LOG_LEVEL_MEDIUM,
+					"PCIConfigSpaceBad",
+					"PCI config space appears to not work");
 			} else
 				fwts_passed(fw, "PCI config space verified");
 
@@ -111,7 +114,7 @@ static int mcfg_test1(fwts_framework *fw)
 {
 	int nr, i;
 	const uint8_t *table_ptr;
-	const uint8_t *table_page;	
+	const uint8_t *table_page;
 	void *mapped_table_page;
 	struct mcfg_entry *table, firstentry;
 	int failed = 0;
@@ -124,7 +127,7 @@ static int mcfg_test1(fwts_framework *fw)
 		page_size = 4096;
 
 	memory_map_name = fwts_memory_map_name(fw->firmware_type);
-	
+
 	fwts_log_info(fw,
 		"This test tries to validate the MCFG table by comparing the first "
 		"16 bytes in the MMIO mapped config space with the 'traditional' config "
@@ -146,21 +149,27 @@ static int mcfg_test1(fwts_framework *fw)
 	mcfg_size -= 8;  /* 8 bytes of padding */
 
 	if ((int)mcfg_size<0) {
-		fwts_failed(fw, LOG_LEVEL_HIGH, "MCFGInvalidSize", "Invalid MCFG ACPI table size: got %d bytes expecting more", mcfg_size + 36 + 8);
+		fwts_failed(fw, LOG_LEVEL_HIGH, "MCFGInvalidSize",
+			"Invalid MCFG ACPI table size: got %d bytes expecting more",
+			mcfg_size + 36 + 8);
 		fwts_tag_failed(fw, FWTS_TAG_ACPI_INVALID_TABLE);
-		fwts_advice(fw, "MCFG table must be least %d bytes (header size) with multiples of %d"
-				"bytes for each MCFG entry.", 36+8, (int)sizeof(struct mcfg_entry));
+		fwts_advice(fw,
+			"MCFG table must be least %d bytes (header size) with "
+			"multiples of %d "bytes for each MCFG entry.",
+			36+8, (int)sizeof(struct mcfg_entry));
 		return FWTS_ERROR;
 	}
 	nr = mcfg_size / sizeof(struct mcfg_entry);
 
 	if (!nr) {
-		fwts_failed(fw, LOG_LEVEL_MEDIUM, "MCFGNoEntries", "No MCFG ACPI table entries");
+		fwts_failed(fw, LOG_LEVEL_MEDIUM, "MCFGNoEntries",
+			"No MCFG ACPI table entries");
 		return FWTS_ERROR;
 	}
 
 	if ((nr * sizeof(struct mcfg_entry)) != mcfg_size) {
-		fwts_failed(fw, LOG_LEVEL_HIGH, "MCFGInvalidSize2", "MCFG table is not a multiple of record size");
+		fwts_failed(fw, LOG_LEVEL_HIGH, "MCFGInvalidSize2",
+			"MCFG table is not a multiple of record size");
 		fwts_tag_failed(fw, FWTS_TAG_ACPI_INVALID_TABLE);
 		return FWTS_ERROR;
 	}
@@ -171,7 +180,8 @@ static int mcfg_test1(fwts_framework *fw)
 	table_page = table_ptr = (const uint8_t *)mcfg_table->data;
 
 	if (table_page == NULL) {
-		fwts_failed(fw, LOG_LEVEL_HIGH, "MCFGInvalidTable", "Invalid MCFG ACPI table");
+		fwts_failed(fw, LOG_LEVEL_HIGH, "MCFGInvalidTable",
+			"Invalid MCFG ACPI table");
 		fwts_tag_failed(fw, FWTS_TAG_ACPI_INVALID_TABLE);
 		return FWTS_ERROR;
 	}
@@ -182,24 +192,27 @@ static int mcfg_test1(fwts_framework *fw)
 	firstentry = *table;
 
 	if (memory_map_list == NULL)
-		fwts_failed(fw, LOG_LEVEL_MEDIUM, "MMapUnreadable", "Cannot check MCFG mmio space against memory map table: could not read memory map table.");
+		fwts_failed(fw, LOG_LEVEL_MEDIUM, "MMapUnreadable",
+			"Cannot check MCFG mmio space against memory map table: could not read memory map table.");
 
 	for (i = 0; i<nr; i++) {
 		fwts_log_info(fw, "Entry address : 0x%x\n", table->low_address);
 
-		if ((memory_map_list != NULL) && (!fwts_memory_map_is_reserved(memory_map_list, table->low_address))) {
-			
+		if ((memory_map_list != NULL) &&
+		    (!fwts_memory_map_is_reserved(memory_map_list, table->low_address))) {
+
 			fwts_failed(fw, LOG_LEVEL_MEDIUM, "MCFGMMIONotReserved",
 				"MCFG mmio config space at 0x%x is not reserved in the memory map table", table->low_address);
 			fwts_tag_failed(fw, FWTS_TAG_BIOS);
-			fwts_advice(fw, "The PCI Express specification states that the PCI Express configuration space should "
-					"be defined in the MCFG table and *maybe* optionally defined in the %s "
-					"if ACPI MCFG is present. "
-					"Linux checks if the region is reserved in the memory map table and will reject the "
-					"MMCONFIG if there is a discrepency between MCFG and the memory map table for the "
-					"PCI Express region.  [See arch/x86/pci/mmconfig-shared.c pci_mmcfg_reject_broken()]. "
-					"It is recommended that this is defined in the %s table for Linux.",
-					memory_map_name, memory_map_name);
+			fwts_advice(fw,
+				"The PCI Express specification states that the PCI Express configuration space should "
+				"be defined in the MCFG table and *maybe* optionally defined in the %s "
+				"if ACPI MCFG is present. "
+				"Linux checks if the region is reserved in the memory map table and will reject the "
+				"MMCONFIG if there is a discrepency between MCFG and the memory map table for the "
+				"PCI Express region.  [See arch/x86/pci/mmconfig-shared.c pci_mmcfg_reject_broken()]. "
+				"It is recommended that this is defined in the %s table for Linux.",
+				memory_map_name, memory_map_name);
 			failed++;
 		}
 
@@ -221,7 +234,7 @@ static int mcfg_test1(fwts_framework *fw)
 	compare_config_space(fw, firstentry.segment, 0, (unsigned char *)mapped_table_page);
 
 	fwts_munmap(mapped_table_page, page_size);
-	
+
 	return FWTS_OK;
 }
 

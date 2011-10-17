0edfdf1b6f85ffe857a0a20a5dab5cdbc2557c83
Colin Ian King <colin.king@canonical.com>
2011-10-17 10:52:51 +0100

    bios: smbios: check header fully and dump fields (LP: #876331)
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- src/bios/smbios/smbios.c
+++ src/bios/smbios/smbios.c
@@ -34,14 +34,14 @@ typedef struct {
 	uint8_t		revision;
 	uint8_t		formatted_area[5];
 	uint8_t		anchor_string[5];
-	uint8_t		intermedate_checksum;
+	uint8_t		intermediate_checksum;
 	uint16_t	struct_table_length;
 	uint32_t	struct_table_address;
 	uint16_t	number_smbios_structures;
 	uint8_t		smbios_bcd_revision;
 }  __attribute__ ((packed)) fwts_smbios_entry;
 
-void *fwts_smbios_find_entry_in_uefi(fwts_framework *fw, fwts_smbios_entry *entry)
+void *smbios_find_entry_in_uefi(fwts_framework *fw, fwts_smbios_entry *entry)
 {
 	void *addr;
 	fwts_smbios_entry *mapped_entry;
@@ -57,7 +57,7 @@ void *fwts_smbios_find_entry_in_uefi(fwts_framework *fw, fwts_smbios_entry *entr
 	return addr;
 }
 
-void *fwts_smbios_find_entry_in_bios(fwts_framework *fw, fwts_smbios_entry *entry)
+void *smbios_find_entry_in_bios(fwts_framework *fw, fwts_smbios_entry *entry)
 {
 	uint8_t *mem;
 	void 	*addr = NULL;
@@ -72,16 +72,8 @@ void *fwts_smbios_find_entry_in_bios(fwts_framework *fw, fwts_smbios_entry *entr
 		if ((*(mem+i)   == '_') &&
 		    (*(mem+i+1) == 'S') &&
 		    (*(mem+i+2) == 'M') &&
-		    (*(mem+i+3) == '_')) {
-			//fwts_smbios_entry *smbios;
-			uint8_t checksum = 0;
-			int j;
-
-			for (j=i;j<i+16;j++)
-				checksum += *(mem+j);
-
-			//smbios = (fwts_smbios_entry*)(mem+i);
-
+		    (*(mem+i+3) == '_') && 
+		    (fwts_checksum(mem + i, 16) == 0)) {
 			addr = (void*)SMBIOS_REGION_START + i;
 			memcpy(entry, (void*)(mem + i), sizeof(fwts_smbios_entry));
 			break;
@@ -93,14 +85,38 @@ void *fwts_smbios_find_entry_in_bios(fwts_framework *fw, fwts_smbios_entry *entr
 	return addr;
 }
 
-static void *fwts_smbios_find_entry(fwts_framework *fw, fwts_smbios_entry *entry)
+static void smbios_dump_entry(fwts_framework *fw, fwts_smbios_entry *entry)
+{
+	fwts_log_info_verbatum(fw, "  Anchor String          : %4.4s", entry->signature);
+	fwts_log_info_verbatum(fw, "  Checksum               : 0x%2.2x", entry->checksum);
+	fwts_log_info_verbatum(fw, "  Entry Point Length     : 0x%2.2x", entry->length);
+	fwts_log_info_verbatum(fw, "  Major Version          : 0x%2.2x", entry->major_version);
+	fwts_log_info_verbatum(fw, "  Minor Version          : 0x%2.2x", entry->minor_version);
+	fwts_log_info_verbatum(fw, "  Maximum Struct Size    : 0x%2.2x", entry->max_struct_size);
+	fwts_log_info_verbatum(fw, "  Entry Point Revision   : 0x%2.2x", entry->revision);
+	fwts_log_info_verbatum(fw, "  Formatted Area         : 0x%2.2x 0x%2.2x 0x%2.2x 0x%2.2x 0x%2.2x", 
+			entry->formatted_area[0], entry->formatted_area[1],
+			entry->formatted_area[2], entry->formatted_area[3],
+			entry->formatted_area[4]);
+	fwts_log_info_verbatum(fw, "  Intermediate Anchor    : %5.5s", (char *)entry->anchor_string);
+	fwts_log_info_verbatum(fw, "  Intermediate Checksum  : 0x%2.2x", entry->intermediate_checksum);
+	fwts_log_info_verbatum(fw, "  Structure Table Length : 0x%4.4x", entry->struct_table_length);
+	fwts_log_info_verbatum(fw, "  Structure Table Address: 0x%8.8x", entry->struct_table_address);
+	fwts_log_info_verbatum(fw, "  # of SMBIOS Structures : 0x%4.4x", entry->number_smbios_structures);
+	fwts_log_info_verbatum(fw, "  SBMIOS BCD Revision    : %2.2x", entry->smbios_bcd_revision);
+	if (entry->smbios_bcd_revision == 0) {
+		fwts_log_info_verbatum(fw, "    BCD Revision 00 indicates compliance with specification stated in Major/Minor Version.");
+	}
+}
+
+static void *smbios_find_entry(fwts_framework *fw, fwts_smbios_entry *entry)
 {
 	switch (fw->firmware_type) {
 	case FWTS_FIRMWARE_BIOS:
-		return fwts_smbios_find_entry_in_bios(fw, entry);
+		return smbios_find_entry_in_bios(fw, entry);
 		break;
 	case FWTS_FIRMWARE_UEFI:
-		return fwts_smbios_find_entry_in_uefi(fw, entry);
+		return smbios_find_entry_in_uefi(fw, entry);
 		break;
 	default:
 		return NULL;
@@ -116,20 +132,50 @@ static int smbios_test1(fwts_framework *fw)
 	fwts_log_info(fw,
 		"This test tries to find and sanity check the SMBIOS "
 		"data structures.");
-	if ((addr = fwts_smbios_find_entry(fw, &entry)) == NULL)
+	if ((addr = smbios_find_entry(fw, &entry)) == NULL)
 		fwts_failed(fw, LOG_LEVEL_MEDIUM,
 			"SMBIOSNoEntryPoint",
 			"Could not find SMBIOS Table Entry Point.");
 	else {
+		static char warning[] = 
+			"This field is not checked by the kernel, and so will not affect the system, "
+			"however it should be fixed to conform to the latest version of the "
+			"System Management BIOS (SMBIOS) Reference Specification. ";
+
 		fwts_passed(fw, "Found SMBIOS Table Entry Point at %p", addr);
-		/* TODO: Dump out table */
+
+		smbios_dump_entry(fw, &entry);
+		
+		if (entry.length != 0x1f) {
+			fwts_failed(fw, LOG_LEVEL_LOW,
+				"SMBIOSBadEntryLength",
+				"SMBIOS Table Entry Point Length is 0x%2.2x, should be 0x1f", entry.length);
+			fwts_advice(fw, "%s Note that version 2.1 of the specification incorrectly stated that the "
+				"Entry Point Length should be 0x1e when it should be 0x1f.", warning);
+		}
+		if (memcmp(entry.anchor_string, "_DMI_", 5) != 0) {
+			fwts_failed(fw, LOG_LEVEL_HIGH,
+				"SMBIOSBadIntermediateAnchor", 
+				"SMBIOS Table Entry Intermediate Anchor String was '%5.5s' and should be '_DMI_'.", 
+					entry.anchor_string);
+			fwts_advice(fw, "%s", warning);
+		}
+
+		if ((entry.struct_table_length > 0) && (entry.struct_table_address == 0)) {
+			fwts_failed(fw, LOG_LEVEL_HIGH,
+				"SMBIOSBadTableAddress",
+				"SMBIOS Table Entry Structure Table Address is NULL and should be defined.");
+			fwts_advice(fw,
+				"The address of the SMBIOS Structure Tables must be defined if the "
+				"length of these tables is defined.");
+		}
 	}
 
 	return FWTS_OK;
 }
 
 static fwts_framework_minor_test smbios_tests[] = {
-	{ smbios_test1, "Find and SMBIOS Table Entry Point." },
+	{ smbios_test1, "Find and Check SMBIOS Table Entry Point." },
 	{ NULL, NULL }
 };
 

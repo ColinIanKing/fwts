37deb5e019dbd62e86b273a9cd7d9814830b6546
Colin Ian King <colin.king@canonical.com>
2011-10-17 09:23:57 +0100

    bios: smbios: use systab efi helper
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- src/bios/smbios/smbios.c
+++ src/bios/smbios/smbios.c
@@ -41,47 +41,31 @@ typedef struct {
 	uint8_t		smbios_bcd_revision;
 }  __attribute__ ((packed)) fwts_smbios_entry;
 
-static int fwts_smbios_find_entry_in_uefi(fwts_framework *fw,
-	fwts_smbios_entry *entry, uint32_t *addr)
+void *fwts_smbios_find_entry_in_uefi(fwts_framework *fw, fwts_smbios_entry *entry)
 {
-	fwts_list *systab;
-	fwts_list_link *item;
-	int ret = FWTS_ERROR;
+	void *addr;
+	fwts_smbios_entry *mapped_entry;
 
-	*addr = 0;
-
-	if (((systab = fwts_file_open_and_read("/sys/firmware/efi/systab")) == NULL) &&
-	    ((systab = fwts_file_open_and_read("/proc/efi/systab")) == NULL)) {
-		fwts_log_error(fw, "Cannot read UEFI systab file.");
-		return FWTS_ERROR;
-	}
-
-	fwts_list_foreach(item, systab) {
-		char *str = fwts_list_data(char *, item);
-		if (strstr(str, "SMBIOS")) {
-			char *ptr = strstr(str, "=");
-			if (ptr) {
-				*addr = (uint32_t)strtoul(ptr+1, NULL, 0);
-				ret = FWTS_OK;
-				break;
-			}
+	if ((addr = fwts_scan_efi_systab("SMBIOS")) != NULL) {
+		if ((mapped_entry = fwts_mmap((off_t)addr, sizeof(fwts_smbios_entry))) == FWTS_MAP_FAILED) {
+			fwts_log_error(fw, "Cannot mmap SMBIOS entry at %p\n", addr);
+			return NULL;
 		}
+		*entry = *mapped_entry;
+		(void)fwts_munmap(mapped_entry, sizeof(fwts_smbios_entry));
 	}
-	fwts_list_free(systab, free);
-
-	return ret;
+	return addr;
 }
 
-static int fwts_smbios_find_entry_in_bios(fwts_framework *fw,
-	fwts_smbios_entry *entry, uint32_t *addr)
+void *fwts_smbios_find_entry_in_bios(fwts_framework *fw, fwts_smbios_entry *entry)
 {
-	int ret = FWTS_ERROR;
 	uint8_t *mem;
-	int i;
+	void 	*addr = NULL;
+	int 	i;
 
         if ((mem = fwts_mmap(SMBIOS_REGION_START, SMBIOS_REGION_SIZE)) == FWTS_MAP_FAILED) {
 		fwts_log_error(fw, "Cannot mmap SMBIOS region.");
-		return FWTS_ERROR;
+		return NULL;
 	}
 
 	for (i=0; i<SMBIOS_REGION_SIZE; i+= 16) {
@@ -98,48 +82,46 @@ static int fwts_smbios_find_entry_in_bios(fwts_framework *fw,
 
 			//smbios = (fwts_smbios_entry*)(mem+i);
 
-			*addr = SMBIOS_REGION_START + i;
+			addr = (void*)SMBIOS_REGION_START + i;
 			memcpy(entry, (void*)(mem + i), sizeof(fwts_smbios_entry));
-			ret = FWTS_OK;
 			break;
 		}
 	}
 
         (void)fwts_munmap(mem, SMBIOS_REGION_SIZE);
 
-	return ret;
+	return addr;
 }
 
-static int fwts_smbios_find_entry(fwts_framework *fw,
-	fwts_smbios_entry *entry, uint32_t *addr)
+static void *fwts_smbios_find_entry(fwts_framework *fw, fwts_smbios_entry *entry)
 {
 	switch (fw->firmware_type) {
 	case FWTS_FIRMWARE_BIOS:
-		return fwts_smbios_find_entry_in_bios(fw, entry, addr);
+		return fwts_smbios_find_entry_in_bios(fw, entry);
 		break;
 	case FWTS_FIRMWARE_UEFI:
-		return fwts_smbios_find_entry_in_uefi(fw, entry, addr);
+		return fwts_smbios_find_entry_in_uefi(fw, entry);
 		break;
 	default:
-		return FWTS_ERROR;
+		return NULL;
 		break;
 	}
 }
 
 static int smbios_test1(fwts_framework *fw)
 {
-	uint32_t addr = 0;
+	void *addr = 0;
 	fwts_smbios_entry entry;
 
 	fwts_log_info(fw,
 		"This test tries to find and sanity check the SMBIOS "
 		"data structures.");
-	if (fwts_smbios_find_entry(fw, &entry, &addr) != FWTS_OK)
+	if ((addr = fwts_smbios_find_entry(fw, &entry)) == NULL)
 		fwts_failed(fw, LOG_LEVEL_MEDIUM,
 			"SMBIOSNoEntryPoint",
 			"Could not find SMBIOS Table Entry Point.");
 	else {
-		fwts_passed(fw, "Found SMBIOS Table Entry Point at 0x%8.8x", addr);
+		fwts_passed(fw, "Found SMBIOS Table Entry Point at %p", addr);
 		/* TODO: Dump out table */
 	}
 

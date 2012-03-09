d9674b57513e5f82ca4c7b7dd1fab4142254b9e7
Keng-Yu Lin <kengyu@canonical.com>
2012-03-09 15:10:20 +0800

    lib: fwts_version.h - update to V0.24.21
    Signed-off-by: Keng-Yu Lin <kengyu@canonical.com>

--- src/lib/include/fwts_version.h
+++ src/lib/include/fwts_version.h
@@ -16,5 +16,5 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  */
-#define FWTS_VERSION "V0.24.20"
-#define FWTS_DATE    "Fri Feb 10 09:39:03 CST 2012"
+#define FWTS_VERSION "V0.24.21"
+#define FWTS_DATE    "Fri Mar 9 15:10:20 CST 2012"

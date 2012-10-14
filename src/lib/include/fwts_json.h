5eba577ea14bf26f32adfc416583ed60b76aab2f
Colin Ian King <colin.king@canonical.com>
2012-10-14 11:16:06 +0000

    lib: fwts_json: wrapper for json error returns
    Older versions of json-c may return an error in an
    object as a ((json_object*)-1), where as newer
    versions return NULL.  To cope with this API change
    we create a macro FWTS_JSON_ERROR that will return
    true if an object pointer is flagging an error
    condition as a NULL or a ((json_object*)-1) pointer.
    This way we can cope with errors no matter which
    version of json-c we use.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Keng-Yu Lin <kengyu@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>

---
+++ src/lib/include/fwts_json.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2012 Canonical
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef __FWTS_JSON_H__
+#define __FWTS_JSON_H__
+
+#include <json/json.h>
+
+#define __FWTS_JSON_ERR_PTR__ ((json_object*) -1)
+/*
+ *  Older versions of json-c may return an error in an
+ *  object as a ((json_object*)-1), where as newer
+ *  versions return NULL, so check for these. Sigh.
+ */
+#define FWTS_JSON_ERROR(ptr) \
+	( (ptr == NULL) || ((json_object*)ptr == __FWTS_JSON_ERR_PTR__) )
+
+#endif

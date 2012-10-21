42b23a734ae0f0111fe981b97cf5b2284d61690a
IvanHu <ivan.hu@canonical.com>
2012-10-21 21:29:27 +0000

    lib: fwts_efi_module: add fwts library helper fuctions for efi_runtime module
    These helper fuctions are used for loading, unloading and checking
    the existence of efi_euntime module.
    
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>

---
+++ src/lib/include/fwts_efi_module.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2012 Canonical
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef __FWTS_EFI_MODULE_H__
+#define __FWTS_EFI_MODULE_H__
+
+int fwts_lib_efi_runtime_load_module(fwts_framework *fw);
+int fwts_lib_efi_runtime_unload_module(fwts_framework *fw);
+
+#endif
---
+++ src/lib/src/fwts_efi_module.c
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2012 Canonical
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "fwts_pipeio.h"
+
+static bool module_already_loaded = false;
+
+static int check_module_loaded(void)
+{
+	FILE *fp;
+	char buffer[1024];
+
+	module_already_loaded = false;
+	if ((fp = fopen("/proc/modules", "r")) != NULL) {
+		while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+			if (strstr(buffer, "efi_runtime") != NULL) {
+				module_already_loaded = true;
+				break;
+			}
+		}
+		fclose(fp);
+		return FWTS_OK;
+	}
+	return FWTS_ERROR;
+}
+
+int fwts_lib_efi_runtime_load_module(fwts_framework *fw)
+{
+	struct stat statbuf;
+	fwts_list *output;
+
+	if (check_module_loaded() != FWTS_OK) {
+		fwts_log_error(fw, "Could not open /proc/modules for checking module loaded.");
+		return FWTS_ERROR;
+	}
+
+	if (!module_already_loaded) {
+		if (fwts_pipe_exec("modprobe efi_runtime", &output) != FWTS_OK) {
+			fwts_log_error(fw, "Load efi_runtime module error.");
+			return FWTS_ERROR;
+		} else {
+			if (output)
+				fwts_text_list_free(output);
+			(void)check_module_loaded();
+			if (!module_already_loaded) {
+				fwts_log_error(fw, "Could not load efi_runtime module.");
+				return FWTS_ERROR;
+			}
+		}
+	}
+
+	if (stat("/dev/efi_runtime", &statbuf)) {
+		fwts_log_error(fw, "Loaded efi_runtime module but /dev/efi_runtime is not present.");
+		return FWTS_ERROR;
+	}
+
+	if (!S_ISCHR(statbuf.st_mode)) {
+		fwts_log_error(fw, "Loaded efi_runtime module but /dev/efi_runtime is not a char device.");
+		return FWTS_ERROR;
+	}
+
+	return FWTS_OK;
+}
+
+
+int fwts_lib_efi_runtime_unload_module(fwts_framework *fw)
+{
+	fwts_list *output;
+
+	if (check_module_loaded() != FWTS_OK) {
+		fwts_log_error(fw, "Could not open /proc/modules for checking module loaded.");
+		return FWTS_ERROR;
+	}
+	if (module_already_loaded) {
+		if (fwts_pipe_exec("modprobe -r efi_runtime", &output) != FWTS_OK) {
+			fwts_log_error(fw, "Unload efi_runtime module error.");
+			return FWTS_ERROR;
+		} else {
+			if (output)
+				fwts_text_list_free(output);
+			(void)check_module_loaded();
+			if (module_already_loaded) {
+				fwts_log_error(fw, "Could not unload efi_runtime module.");
+				return FWTS_ERROR;
+			}
+		}
+	}
+
+	return FWTS_OK;
+}

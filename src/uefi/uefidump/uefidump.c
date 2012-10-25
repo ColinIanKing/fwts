ColinIanKing/fwts

commit cdd817599787055dd4a20def83d4853b62ee854c

lib + tests: bulk rename of fwts flag macros
The FWTS_FRAMEWORK_FLAGS_ macros were originally really rather too long
to type, so there were a bunch of #defines to shorten these for common
use in just the fwts tests.  However, the aliasing of these is really not
a good idea after all as it leads to confusing mixes of the same value
with different names, especially in the fwts framework, for example

FWTS_BATCH is really an alias of FWTS_FRAMEWORK_FLAGS_BATCH (urgh!)

This patch simplifies the matter by renaming all these macros from
FWTS_FRAME_WORK_FLAGS* to FWTS_FLAG_* and we replace the bad short cut
versions to the FWTS_FLAG_* equivalent.

This changes no functionality in the code whatsoever, it just tidies
it up and makes it more understandable.

Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Keng-Yu Lin <kengyu@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>

src/uefi/uefidump/uefidump.c
@@ -767,4 +767,4 @@ static fwts_framework_ops uefidump_ops = {
 	.minor_tests = uefidump_tests
 };
 
-FWTS_REGISTER(uefidump, &uefidump_ops, FWTS_TEST_ANYTIME, FWTS_UTILS | FWTS_ROOT_PRIV);
+FWTS_REGISTER(uefidump, &uefidump_ops, FWTS_TEST_ANYTIME, FWTS_FLAG_UNSAFE | FWTS_FLAG_ROOT_PRIV);

src/acpi/ac_adapter/ac_adapter.c
@@ -161,6 +161,6 @@ static fwts_framework_ops ac_adapter_ops = {
 	.minor_tests = ac_adapter_tests
 };
 
-FWTS_REGISTER(ac_adapter, &ac_adapter_ops, FWTS_TEST_ANYTIME, FWTS_INTERACTIVE);
+FWTS_REGISTER(ac_adapter, &ac_adapter_ops, FWTS_TEST_ANYTIME, FWTS_FLAG_INTERACTIVE);
 
 #endif

src/acpi/acpidump/acpidump.c
@@ -1694,4 +1694,4 @@ static fwts_framework_ops acpidump_ops = {
 	.minor_tests = acpidump_tests
 };
 
-FWTS_REGISTER(acpidump, &acpidump_ops, FWTS_TEST_ANYTIME, FWTS_UTILS);
+FWTS_REGISTER(acpidump, &acpidump_ops, FWTS_TEST_ANYTIME, FWTS_FLAG_UTILS);

src/acpi/acpiinfo/acpiinfo.c
@@ -247,6 +247,6 @@ static fwts_framework_ops acpiinfo_ops = {
 	.minor_tests = acpiinfo_tests
 };
 
-FWTS_REGISTER(acpiinfo, &acpiinfo_ops, FWTS_TEST_EARLY, FWTS_BATCH);
+FWTS_REGISTER(acpiinfo, &acpiinfo_ops, FWTS_TEST_EARLY, FWTS_FLAG_BATCH);
 
 #endif

src/acpi/acpitables/acpitables.c
@@ -468,4 +468,4 @@ static fwts_framework_ops acpi_table_check_ops = {
 	.minor_tests = acpi_table_check_tests
 };
 
-FWTS_REGISTER(acpitables, &acpi_table_check_ops, FWTS_TEST_ANYTIME, FWTS_BATCH);
+FWTS_REGISTER(acpitables, &acpi_table_check_ops, FWTS_TEST_ANYTIME, FWTS_FLAG_BATCH);

src/acpi/apicinstance/apicinstance.c
@@ -81,4 +81,4 @@ static fwts_framework_ops apicinstance_ops = {
 	.minor_tests = apicinstance_tests
 };
 
-FWTS_REGISTER(apicinstance, &apicinstance_ops, FWTS_TEST_ANYTIME, FWTS_BATCH);
+FWTS_REGISTER(apicinstance, &apicinstance_ops, FWTS_TEST_ANYTIME, FWTS_FLAG_BATCH);

src/acpi/battery/battery.c
@@ -291,6 +291,6 @@ static fwts_framework_ops battery_ops = {
 	.minor_tests = battery_tests
 };
 
-FWTS_REGISTER(battery, &battery_ops, FWTS_TEST_ANYTIME, FWTS_INTERACTIVE);
+FWTS_REGISTER(battery, &battery_ops, FWTS_TEST_ANYTIME, FWTS_FLAG_INTERACTIVE);
 
 #endif

src/acpi/brightness/brightness.c
@@ -384,6 +384,6 @@ static fwts_framework_ops brightness_ops = {
 	.minor_tests = brightness_tests
 };
 
-FWTS_REGISTER(brightness, &brightness_ops, FWTS_TEST_ANYTIME, FWTS_INTERACTIVE);
+FWTS_REGISTER(brightness, &brightness_ops, FWTS_TEST_ANYTIME, FWTS_FLAG_INTERACTIVE);
 
 #endif

commit dff90f7398d1af494ba018a2f8bfa58f25ed1e34

uefi: uefidump: remove FWTS_FLAG_UNSAFE from test.
Commit cdd817599787055dd4a20def83d4853b62ee854c mistakenly
set this test as an unsafe test.  Set it back to a normal utility
test.

Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Keng-Yu Lin <kengyu@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/uefi/uefidump/uefidump.c
@@ -781,4 +781,4 @@ static fwts_framework_ops uefidump_ops = {
 	.minor_tests = uefidump_tests
 };
 
-FWTS_REGISTER(uefidump, &uefidump_ops, FWTS_TEST_ANYTIME, FWTS_FLAG_UNSAFE | FWTS_FLAG_ROOT_PRIV);
+FWTS_REGISTER(uefidump, &uefidump_ops, FWTS_TEST_ANYTIME, FWTS_FLAG_UTILS | FWTS_FLAG_ROOT_PRIV);
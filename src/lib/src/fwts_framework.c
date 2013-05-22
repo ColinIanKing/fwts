ColinIanKing/fwts

commit 8f551a8719f0b0261076f5d873f2fa709008d325

lib: fwts_framework: ensure we don't truncate null names
Coverity CID #997295: Dereference after null check

minor_test->name may be null, so the call
fwts_framework_strtrunc(namebuf, minor_test->name, sizeof(namebuf))
can cause problems.  Make fwts_framework_strtrunc handle null strings.

Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Keng-Yu Lin <kengyu@canonical.com>

src/lib/src/fwts_framework.c
@@ -291,7 +291,10 @@ static void fwts_framework_show_tests(fwts_framework *fw, bool full)
  */
 static void fwts_framework_strtrunc(char *dest, const char *src, size_t max)
 {
-	strncpy(dest, src, max);
+	if (src)
+		strncpy(dest, src, max);
+	else
+		strncpy(dest, "", max);
 
 	if ((strlen(src) > max) && (max > 3)) {
 		dest[max-1] = 0;

commit aade76f3569bcecfbc60f7660992049e3c737c1b

lib: framework: don't check for long string if src is NULL
Commit 8f551a8719f0b0261076f5d873f2fa709008d325 introduced a new
bug.  We must no check for long strings if src is NULL.

Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/lib/src/fwts_framework.c
@@ -291,16 +291,15 @@ static void fwts_framework_show_tests(fwts_framework *fw, bool full)
  */
 static void fwts_framework_strtrunc(char *dest, const char *src, size_t max)
 {
-	if (src)
+	if (src) {
 		strncpy(dest, src, max);
-	else
+		if ((max > 3) && (strlen(src) > max)) {
+			dest[max-1] = 0;
+			dest[max-2] = '.';
+			dest[max-3] = '.';
+		}
+	} else
 		strncpy(dest, "", max);
-
-	if ((strlen(src) > max) && (max > 3)) {
-		dest[max-1] = 0;
-		dest[max-2] = '.';
-		dest[max-3] = '.';
-	}
 }
 
 /*
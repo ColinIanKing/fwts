08614bfff7783e49394a9166fcb3999702a111d0
Alex Hung <alex.hung@canonical.com>
2014-01-24 11:21:43 +0800

    lib: fwts_version.h - update to V14.01.01
    Signed-off-by: Alex Hung <alex.hung@canonical.com>

--- src/lib/include/fwts_version.h
+++ src/lib/include/fwts_version.h
@@ -16,5 +16,5 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  */
-#define FWTS_VERSION "V14.01.00"
-#define FWTS_DATE    "2014-01-14 08:24:34"
+#define FWTS_VERSION "V14.01.01"
+#define FWTS_DATE    "2014-01-24 03:21:43"

0b131a3234ce733890d2fadfd5c0920a1b9c81da
Colin Ian King <colin.king@canonical.com>
2014-03-24 11:13:05 +0000

    fwts-test: arg-log-format-0001: fix aarch64 failures
    aarch64 fails on this test because the line numbers are different
    from x86 because of a different header length that gets wrapped
    around on lines.  So make the test line number agnostic with some
    sed replacements.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>

--- fwts-test/arg-log-format-0001/test-0003.sh
+++ fwts-test/arg-log-format-0001/test-0003.sh
@@ -3,16 +3,19 @@
 TEST="Test --log-format, %line field"
 NAME=test-0003.sh
 TMPLOG=$TMP/klog.log.$$
+TMPLOG_ORIG=$TMP/klog-0003.log.$$
 TODAY=`date +%d/%m/%y`
 
-$FWTS -w 80 -j $FWTSTESTDIR/../data --klog=$FWTSTESTDIR/arg-log-format-0001/klog.txt --log-format="%owner %line: " klog - | grep "^klog" | sed sx12\/10\/11x${TODAY}x > $TMPLOG
-diff $TMPLOG $FWTSTESTDIR/arg-log-format-0001/klog-0003.log >> $FAILURE_LOG
+$FWTS -w 80 -j $FWTSTESTDIR/../data --klog=$FWTSTESTDIR/arg-log-format-0001/klog.txt --log-format="%owner %line: " klog - | grep "^klog" | sed sx12\/10\/11x${TODAY}x | sed "sx[0-9][0-9][0-9][0-9][0-9]:xXXXXX:x" > $TMPLOG
+sed "sx[0-9][0-9][0-9][0-9][0-9]:xXXXXX:x" < $FWTSTESTDIR/arg-log-format-0001/klog-0003.log > $TMPLOG_ORIG
+diff $TMPLOG $TMPLOG_ORIG >> $FAILURE_LOG
 ret=$?
 if [ $ret -eq 0 ]; then 
 	echo PASSED: $TEST, $NAME
 else
 	echo FAILED: $TEST, $NAME
+	cat $FAILURE_LOG
 fi
 
-rm $TMPLOG
+rm $TMPLOG $TMPLOG_ORIG
 exit $ret

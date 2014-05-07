fb5385fec1f3468b479e87c0bba38ab76f60915b
Ivan Hu <ivan.hu@canonical.com>
2014-05-07 09:57:17 +0800

    lib: fwts_version.h - update to V14.05.00
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

--- src/lib/include/fwts_version.h
+++ src/lib/include/fwts_version.h
@@ -16,5 +16,5 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  */
-#define FWTS_VERSION "V14.03.01"
-#define FWTS_DATE    "2014-03-27 02:14:17"
+#define FWTS_VERSION "V14.05.00"
+#define FWTS_DATE    "2014-05-07 01:57:17"

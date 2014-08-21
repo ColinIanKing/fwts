565c3126183e1c826adc4f8df398635bb8785d0e
Yang Kun (YK) <kun.yang@canonical.com>
2014-08-21 20:52:46 +0800

    lib: fwts_version.h - update to V14.08.00
    Signed-off-by: Yang Kun (YK) <kun.yang@canonical.com>

--- src/lib/include/fwts_version.h
+++ src/lib/include/fwts_version.h
@@ -16,5 +16,5 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  */
-#define FWTS_VERSION "V14.07.00"
-#define FWTS_DATE    "2014-07-30 11:12:29"
+#define FWTS_VERSION "V14.08.00"
+#define FWTS_DATE    "2014-08-21 12:52:46"

40b00805b1e16a4f4e56240da8b25f839a436f64
Colin Ian King <colin.king@canonical.com>
2014-11-13 10:56:08 +0000

    fwts-test: fix disassemble test to work with latest ACPICA changes
    The "AML Disassembler version" text is now different with the
    latest ACPICA, so grep for a less specific string that matches newer
    and older versions of ACPICA.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>

--- fwts-test/disassemble-0001/test-0001.sh
+++ fwts-test/disassemble-0001/test-0001.sh
@@ -25,8 +25,8 @@ do
 	#
 	#  Remove lines that contain a tmp file output name in disassembly
 	#
-	grep -v "/tmp/fwts_" $TMPDIR/$I.dsl | grep -v "AML Disassembler version" | grep -v "Disassembly of" > $TMPDIR/$I.dsl.fixed.$$
-	grep -v "/tmp/fwts_" $HERE/$I.dsl.original | grep -v "AML Disassembler version" | grep -v "Disassembly of" > $TMPDIR/$I.dsl.orig.fixed.$$
+	grep -v "/tmp/fwts_" $TMPDIR/$I.dsl | grep -v "Disassembler version" | grep -v "Disassembly of" > $TMPDIR/$I.dsl.fixed.$$
+	grep -v "/tmp/fwts_" $HERE/$I.dsl.original | grep -v "Disassembler version" | grep -v "Disassembly of" > $TMPDIR/$I.dsl.orig.fixed.$$
 	
 	diff $TMPDIR/$I.dsl.fixed.$$ $TMPDIR/$I.dsl.orig.fixed.$$
 	if [ $? -eq 0 ]; then 
@@ -39,5 +39,5 @@ do
 	rm $TMPDIR/$I.dsl.fixed.$$ $TMPDIR/$I.dsl.orig.fixed.$$
 done
 
-#rm -rf $TMPDIR $TMPLOG
+rm -rf $TMPDIR $TMPLOG
 exit $failed

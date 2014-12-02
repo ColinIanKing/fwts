c183b553bab3f7d55d18026c5f8cb95233422318
Colin Ian King <colin.king@canonical.com>
2014-12-02 10:17:07 +0000

    acpi: s4: fix infinite loop because wrong variable being incremented
    Fix typo on variable being incremented which leads to a possible
    infinite loop while waiting. Detected by Coverity Scan.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Keng-Yu Lin <kengyu@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>

--- src/acpi/s4/s4.c
+++ src/acpi/s4/s4.c
@@ -425,7 +425,7 @@ static int s4_test_multiple(fwts_framework *fw)
 			tv.tv_usec = (awake_delay % 1000)*1000;
 			select(0, NULL, NULL, NULL, &tv);
 
-			for (j = 0; j < awake_delay / 1000; i++) {
+			for (j = 0; j < awake_delay / 1000; j++) {
 				snprintf(buffer, sizeof(buffer), "(Waiting %d/%d seconds)",
 					j + 1, awake_delay / 1000);
 				fwts_progress_message(fw, percent, buffer);

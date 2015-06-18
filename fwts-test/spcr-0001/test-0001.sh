f35e399097cd5d7516e1978990066da55748d24b
Colin Ian King <colin.king@canonical.com>
2015-06-18 09:49:41 +0100

    fwts-test: Add SPCR test
    Add two tests, one to check for a valid SPCR and another to check
    an invalid SPCR
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Ivan Hu<ivan.hu@canonical.com>

---
+++ fwts-test/spcr-0001/test-0001.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+TEST="Test apcitables against SPCR"
+NAME=test-0001.sh
+TMPLOG=$TMP/spcr.log.$$
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/spcr-0001/acpidump-0001.log spcr - | cut -c7- | grep "^spcr" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/spcr-0001/spcr-0001.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+rm $TMPLOG
+exit $ret

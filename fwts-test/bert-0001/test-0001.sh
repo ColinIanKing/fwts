b6e79c5519f2065eaad1cb581bac3afd3e6dedec
Colin Ian King <colin.king@canonical.com>
2015-06-18 09:49:32 +0100

    fwts-test: Add BERT test
    Add two tests, one to check for a valid BERT and another to check
    an invalid BERT.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Ivan Hu<ivan.hu@canonical.com>

---
+++ fwts-test/bert-0001/test-0001.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+TEST="Test apcitables against BERT"
+NAME=test-0001.sh
+TMPLOG=$TMP/bert.log.$$
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/bert-0001/acpidump-0001.log bert - | cut -c7- | grep "^bert" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/bert-0001/bert-0001.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+#rm $TMPLOG
+exit $ret
---
+++ fwts-test/rsdp-0001/test-0001.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+TEST="Test apcitables against broken ACPI RSDP"
+NAME=test-0001.sh
+TMPLOG=$TMP/rsdp.log.$$
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/rsdp-0001/acpidump-0001.log rsdp - | cut -c7- | grep "^rsdp" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/rsdp-0001/rsdp.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then 
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+#rm $TMPLOG
+exit $ret

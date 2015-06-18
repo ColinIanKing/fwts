a4301135aaefba44b23183ed2cf79039591590dc
Colin Ian King <colin.king@canonical.com>
2015-06-18 09:49:35 +0100

    fwts-test: Add DBGP test
    Add two tests, one to check for a valid DBGP and another to check
    an invalid DBGP.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Ivan Hu<ivan.hu@canonical.com>

---
+++ fwts-test/dbgp-0001/test-0001.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+TEST="Test apcitables against DBGP"
+NAME=test-0001.sh
+TMPLOG=$TMP/dbgp.log.$$
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/dbgp-0001/acpidump-0001.log dbgp - | cut -c7- | grep "^dbgp" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/dbgp-0001/dbgp-0001.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+#rm $TMPLOG
+exit $ret

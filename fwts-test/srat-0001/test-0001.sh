afeb6166d6e868ca111b4bb5a2d2e4f707f43bf2
Colin Ian King <colin.king@canonical.com>
2015-06-18 09:49:39 +0100

    fwts-test: Add SRAT test
    Add two tests, one to check for a valid SRAT and another to check
    an invalid SRAT
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Ivan Hu<ivan.hu@canonical.com>

---
+++ fwts-test/srat-0001/test-0001.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+TEST="Test apcitables against SRAT"
+NAME=test-0001.sh
+TMPLOG=$TMP/srat.log.$$
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/srat-0001/acpidump-0001.log srat - | cut -c7- | grep "^srat" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/srat-0001/srat-0001.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+rm $TMPLOG
+exit $ret

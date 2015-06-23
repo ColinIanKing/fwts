39a30c23c687ab18e44191d5558c48da6b3af510
Colin Ian King <colin.king@canonical.com>
2015-06-23 12:39:40 +0100

    fwts-test: Add regression test for CPEP, update fwst-tests
    Add CPEP test and update arg-show-test* tests to accommodate this
    new test too.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Ivan Hu<ivan.hu@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>

---
+++ fwts-test/cpep-0001/test-0002.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+TEST="Test table against invalid CPEP"
+NAME=test-0001.sh
+TMPLOG=$TMP/cpep.log.$$
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/cpep-0001/acpidump-0002.log cpep - | cut -c7- | grep "^cpep" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/cpep-0001/cpep-0002.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+#rm $TMPLOG
+exit $ret

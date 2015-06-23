36879bfcfda0e7be1ab89ca589a39afc9c047e91
Colin Ian King <colin.king@canonical.com>
2015-06-23 10:54:00 +0100

    fwts-test: Add regression test for BGRT, update fwst-tests
    Add BGRT test and update arg-show-test* tests to accommodate this
    new test too.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Ivan Hu<ivan.hu@canonical.com>

---
+++ fwts-test/bgrt-0001/test-0002.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+TEST="Test apcitables against invalid GBRT"
+NAME=test-0001.sh
+TMPLOG=$TMP/bgrt.log.$$
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/bgrt-0001/acpidump-0002.log bgrt - | cut -c7- | grep "^bgrt" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/bgrt-0001/bgrt-0002.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+rm $TMPLOG
+exit $ret

e5e72fb362954e745a57f35e3327cd64b296ea04
Colin Ian King <colin.king@canonical.com>
2015-06-23 16:12:41 +0100

    fwts-test: Add regression test for FACS, update fwst-tests
    Add FACS test and update arg-show-test* tests to accommodate this
    new test too.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Ivan Hu<ivan.hu@canonical.com>

---
+++ fwts-test/facs-0001/test-0002.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+TEST="Test table against invalid FACS"
+NAME=test-0001.sh
+TMPLOG=$TMP/facs.log.$$
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/facs-0001/acpidump-0002.log facs - | cut -c7- | grep "^facs" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/facs-0001/facs-0002.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+#rm $TMPLOG
+exit $ret

541b6ff153edf379ea5a1a085b9793dff9604ca0
Colin Ian King <colin.king@canonical.com>
2015-06-30 18:20:33 +0100

    fwts-test: Add LPIT table regression test
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Ivan Hu<ivan.hu@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>

---
+++ fwts-test/lpit-0001/test-0002.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+TEST="Test apcitables against invalid LPIT"
+NAME=test-0001.sh
+TMPLOG=$TMP/lpit.log.$$
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/lpit-0001/acpidump-0002.log lpit - | cut -c7- | grep "^lpit" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/lpit-0001/lpit-0002.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+#rm $TMPLOG
+exit $ret

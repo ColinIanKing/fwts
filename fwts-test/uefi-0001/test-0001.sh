1f11318102da9089729555d13a875650b797a6d0
Ivan Hu <ivan.hu@canonical.com>
2015-07-07 14:44:59 +0800

    fwts-test: Add UEFI ACPI data table regression tests
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>

---
+++ fwts-test/uefi-0001/test-0001.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+TEST="Test table against UEFI"
+NAME=test-0001.sh
+TMPLOG=$TMP/uefi.log.$$
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/uefi-0001/acpidump-0001.log uefi - | cut -c7- | grep "^uefi" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/uefi-0001/uefi-0001.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+rm $TMPLOG
+exit $ret

ColinIanKing/fwts

commit 6e9c327a42017de933fdfe72cb7f96d936a1a488

lib: Allow reading ACPI dump file by non-root user
Signed-off-by: Anthony Wong <anthony.wong@canonical.com>
Acked-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Ivan Hu<ivan.hu@canonical.com>

src/lib/src/fwts_dump.c
@@ -170,10 +170,11 @@ static int dump_acpi_tables(fwts_framework *fw)
 	for (i=0;;i++) {
 		fwts_acpi_table_info *table;
 
-		if (fwts_acpi_get_table(fw, i, &table) == FWTS_ERROR) {
+		int ret = fwts_acpi_get_table(fw, i, &table);
+		if (ret != FWTS_OK) {
 			fprintf(stderr, "Cannot read ACPI tables.\n");
 			fclose(fp);
-			return FWTS_ERROR;
+			return ret;
 		}
 		if (table == NULL)
 			break;
@@ -290,13 +291,17 @@ int fwts_dump_info(fwts_framework *fw)
 	else
 		printf("Dumped lspci data to lspci.log\n");
 
-	if (root_priv) {
-		if (dump_acpi_tables(fw) != FWTS_OK)
-			fprintf(stderr, "Failed to dump ACPI tables.\n");
-		else
+	switch (dump_acpi_tables(fw)) {
+		case FWTS_OK:
 			printf("Dumped ACPI tables to acpidump.log\n");
-	} else
-		fprintf(stderr, "Need root privilege to dump ACPI tables.\n");
+			break;
+		case FWTS_ERROR_NO_PRIV:
+			fprintf(stderr, "Need root privilege to dump ACPI tables.\n");
+			break;
+		default:
+			fprintf(stderr, "Failed to dump ACPI tables.\n");
+			break;
+	}
 
 	if (dump_cpuinfo() != FWTS_OK)
 		fprintf(stderr, "Failed to dump cpuinfo.\n");

commit 5261cbf555f20cc16a740f9644e1f4bf967719a6

lib: fix build error on non-Intel platforms
Fix for build error:
  fwts_dump.c: In function 'fwts_dump_info':
  fwts_dump.c:259:7: error: unused variable 'root_priv' [-Werror=unused-variable]
  cc1: all warnings being treated as errors

Fixes: 6e9c327a42017de ("lib: Allow reading ACPI dump file by non-root user")
Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Ivan Hu<ivan.hu@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>

src/lib/src/fwts_dump.c
@@ -256,7 +256,6 @@ static int dump_cpuinfo(void)
 int fwts_dump_info(fwts_framework *fw)
 {
 	char path[PATH_MAX+1];
-	bool root_priv = (fwts_check_root_euid(fw, false) == FWTS_OK);
 
 	if (getcwd(path, PATH_MAX) == NULL)
 		strcpy(path, "./");
@@ -277,7 +276,7 @@ int fwts_dump_info(fwts_framework *fw)
 		printf("Dumping dmesg to dmesg.log\n");
 
 #ifdef FWTS_ARCH_INTEL
-	if (root_priv) {
+	if (fwts_check_root_euid(fw, false) == FWTS_OK) {
 		if (dump_dmidecode(fw) != FWTS_OK)
 			fprintf(stderr, "Failed to dump output from dmidecode.\n");
 		else
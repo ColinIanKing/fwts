19542b4b3af0adfb2719f4fe34562419d2dba3a6
Colin Ian King <colin.king@canonical.com>
2015-11-30 20:21:22 +0000

    fwts-test: fix up acpidump releated tests
    fix up tests that checked on acpidump output.  We need to check
    against different output.  I've also tweaked the arg-table-path tests
    to test for wider output and forced using C sort ordering on the test
    data to ensure sort does not pick up the wrong sort order.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>

--- fwts-test/arg-table-path-0001/test-0001.sh
+++ fwts-test/arg-table-path-0001/test-0001.sh
@@ -3,18 +3,16 @@
 TEST="Test --table-path to load dumped tables produced by acpixtract"
 NAME=test-0001.sh
 TMPLOG=$TMP/acpidump.log.$$
-SORTED=$TMP/acpidump.log.sorted
 HERE=$FWTSTESTDIR/arg-table-path-0001
-
 #
 # Unfortunately we can pull in the tables in different order depending
 # on the way the tables are stored in the directory. Since we only care
 # about output content and not necessary table order, we sort and uniq
 # the output before we diff.
 #
-$FWTS --log-format="%line %owner " -w 80 --table-path=$HERE acpidump - | grep "^[0-9]*[ ]*acpidump" | cut -c7- | sort | uniq > $TMPLOG
-sort $HERE/acpidump-0001.log > $SORTED
-diff $TMPLOG $SORTED >> $FAILURE_LOG
+$FWTS --log-format="%line %owner " -w 200 --table-path=$HERE acpidump - | grep "^[0-9]*[ ]*acpidump" | cut -c7- | LC_ALL=C sort | uniq > $TMPLOG
+cat $HERE/acpidump-0001.log | grep "^[0-9]*[ ]*acpidump" | cut -c1- | LC_ALL=C sort | uniq > $TMPLOG.orig
+diff $TMPLOG $TMPLOG.orig >> $FAILURE_LOG
 ret=$?
 if [ $ret -eq 0 ]; then 
 	echo PASSED: $TEST, $NAME
@@ -22,5 +20,5 @@ else
 	echo FAILED: $TEST, $NAME
 fi
 
-rm $TMPLOG $SORTED
+rm $TMPLOG $TMPLOG.orig
 exit $ret

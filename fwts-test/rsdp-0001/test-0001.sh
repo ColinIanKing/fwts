92d5fc873daa4256a4343951f9bd1f20237e5ca2
Colin Ian King <colin.king@canonical.com>
2016-02-19 17:35:34 +0000

    fwts-test: only run rsdp regression test on specific arches (LP: #1547602)
    We don't want to run this regression test on architectures such as
    armhf and ia64 for the moment.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>

--- fwts-test/rsdp-0001/test-0001.sh
+++ fwts-test/rsdp-0001/test-0001.sh
@@ -4,6 +4,16 @@ TEST="Test apcitables against broken ACPI RSDP"
 NAME=test-0001.sh
 TMPLOG=$TMP/rsdp.log.$$
 
+machine=$(uname -m)
+case $machine in
+x86 | x86_32 | x86_64 | i686 | arm64 | aarch64)
+	;;
+*)
+        echo SKIP: $TEST, $NAME
+        exit 77
+	;;
+esac
+
 $FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/rsdp-0001/acpidump-0001.log rsdp - | cut -c7- | grep "^rsdp" > $TMPLOG
 diff $TMPLOG $FWTSTESTDIR/rsdp-0001/rsdp.log >> $FAILURE_LOG
 ret=$?

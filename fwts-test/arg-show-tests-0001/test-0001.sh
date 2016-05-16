05d998a9a76051dd39f862cc26d83102af674ccd
Colin Ian King <colin.king@canonical.com>
2016-05-16 09:30:00 +0100

    fwts-test: arg-show-tests-0001: reset tty after forcing column width
    We should only run this test if we can reset the width to 80 colummns
    and we need to reset the tty width back at the end of the test
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>

--- fwts-test/arg-show-tests-0001/test-0001.sh
+++ fwts-test/arg-show-tests-0001/test-0001.sh
@@ -13,7 +13,16 @@ if [ $? -eq 1 ]; then
 	exit 77
 fi
 
-stty cols 80
+cols=$(stty -a | tr ';' '\n' | grep "columns" | cut -d' ' -f3) 2> /dev/null
+#
+#  If we can't set the tty then we can't test
+#
+stty cols 80 2> /dev/null
+if [ $? -eq 1 ]; then
+        tset 2> /dev/null
+        echo SKIP: $TEST, $NAME
+        exit 77
+fi
 $FWTS -s > $TMPLOG
 diff $TMPLOG $FWTSTESTDIR/arg-show-tests-0001/arg-show-tests-0001.log >> $FAILURE_LOG
 ret=$?
@@ -22,6 +31,9 @@ if [ $ret -eq 0 ]; then
 else
 	echo FAILED: $TEST, $NAME
 fi
+
+stty cols 80 2> /dev/null
+tset 2> /dev/null
 
 rm $TMPLOG
 exit $ret

3bedf13519c01b923a6d83445830c4b7af5f2f42
Alex Hung <alex.hung@canonical.com>
2016-05-18 16:28:17 +0800

    lib: fwts_version.h - update to V16.05.00
    Signed-off-by: Alex Hung <alex.hung@canonical.com>

--- src/lib/include/fwts_version.h
+++ src/lib/include/fwts_version.h
@@ -16,5 +16,5 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  */
-#define FWTS_VERSION "V16.03.00"
-#define FWTS_DATE    "2016-03-14 09:10:20"
+#define FWTS_VERSION "V16.05.00"
+#define FWTS_DATE    "2016-05-18 08:28:17"

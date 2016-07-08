10e7969aa495b34a9b3caf0b382fa701e19a42ff
Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
2016-07-08 16:42:20 -0700

    lib: fwts_cpu: return info struct only when requested CPU is found
    If the requested CPU is not found, we would be returning an empty
    structure (along with NULL pointers). It makes no sense to return
    such useless structure. Instead, return NULL.
    
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>

--- src/lib/src/fwts_cpu.c
+++ src/lib/src/fwts_cpu.c
@@ -106,7 +106,7 @@ fwts_cpuinfo_x86 *fwts_cpu_get_info(const int which_cpu)
 	FILE *fp;
 	char buffer[1024];
 	fwts_cpuinfo_x86 *cpu;
-	int cpu_num = -1;
+	int cpu_num = -1, found = 0;
 
 	if ((cpu = (fwts_cpuinfo_x86*)calloc(1, sizeof(fwts_cpuinfo_x86))) == NULL)
 		return NULL;
@@ -135,6 +135,8 @@ fwts_cpuinfo_x86 *fwts_cpu_get_info(const int which_cpu)
 				continue;
 		}
 
+		found = 1;
+
 		if (!strncmp(buffer, "vendor_id", 9)) {
 			cpu->vendor_id = strdup(ptr);
 			continue;
@@ -161,6 +163,11 @@ fwts_cpuinfo_x86 *fwts_cpu_get_info(const int which_cpu)
 		}
 	}
 	fclose(fp);
+
+	if (!found) {
+		free(cpu);
+		cpu = NULL;
+	}
 
 	return cpu;
 }

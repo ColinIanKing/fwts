3d7ce24d224a7ca35baede5ca2c8dc806decf66e
Ivan Hu <ivan.hu@canonical.com>
2016-07-27 15:43:49 +0800

    lib: fwts_version.h - update to V16.07.00
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

--- src/lib/include/fwts_version.h
+++ src/lib/include/fwts_version.h
@@ -16,5 +16,5 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  */
-#define FWTS_VERSION "V16.06.00"
-#define FWTS_DATE    "2016-06-22 07:48:52"
+#define FWTS_VERSION "V16.07.00"
+#define FWTS_DATE    "2016-07-27 07:43:49"

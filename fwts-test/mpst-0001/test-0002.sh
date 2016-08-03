a0c85e5b9a8151a3af8a3ddeea2ac78b42c8942c
Alex Hung <alex.hung@canonical.com>
2016-08-03 16:13:53 +0800

    fwts-test: Add regression test for MPST
    Signed-off-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>

---
+++ fwts-test/mpst-0001/test-0002.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+TEST="Test table against invalid MPST"
+NAME=test-0001.sh
+TMPLOG=$TMP/mpst.log.$$
+
+$FWTS --show-tests | grep MPST > /dev/null
+if [ $? -eq 1 ]; then
+	echo SKIP: $TEST, $NAME
+	exit 77
+fi
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/mpst-0001/acpidump-0002.log mpst - | cut -c7- | grep "^mpst" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/mpst-0001/mpst-0002.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+rm $TMPLOG
+exit $ret

59b49113b4e5ca622a040f45b89bdb0fcfb676ef
Alex Hung <alex.hung@canonical.com>
2016-08-05 20:07:48 +0800

    fwts-test: Add regression test for PMTT
    Signed-off-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>

---
+++ fwts-test/pmtt-0001/test-0001.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+TEST="Test apcitables against PMTT"
+NAME=test-0001.sh
+TMPLOG=$TMP/pmtt.log.$$
+
+$FWTS --show-tests | grep pmtt > /dev/null
+if [ $? -eq 1 ]; then
+	echo SKIP: $TEST, $NAME
+	exit 77
+fi
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/pmtt-0001/acpidump-0001.log pmtt - | cut -c7- | grep "^pmtt" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/pmtt-0001/pmtt-0001.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+rm $TMPLOG
+exit $ret

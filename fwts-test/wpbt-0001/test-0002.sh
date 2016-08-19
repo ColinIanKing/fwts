edc3d3e8eb191ce0c618e93ee77d9139b1fd1314
Alex Hung <alex.hung@canonical.com>
2016-08-19 12:26:10 +0800

    fwts-test: Add regression test for WPBT
    Signed-off-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>

---
+++ fwts-test/wpbt-0001/test-0002.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+TEST="Test apcitables against invalid WPBT"
+NAME=test-0001.sh
+TMPLOG=$TMP/wpbt.log.$$
+
+$FWTS --show-tests | grep wpbt > /dev/null
+if [ $? -eq 1 ]; then
+	echo SKIP: $TEST, $NAME
+	exit 77
+fi
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/wpbt-0001/acpidump-0002.log wpbt - | cut -c7- | grep "^wpbt" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/wpbt-0001/wpbt-0002.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+rm $TMPLOG
+exit $ret

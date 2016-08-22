1753fe8672cb4211092edadcc8873721a06cdb82
Alex Hung <alex.hung@canonical.com>
2016-08-22 11:25:01 +0800

    fwts-test: Add regression test for DRTM
    Signed-off-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>

---
+++ fwts-test/drtm-0001/test-0002.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+TEST="Test apcitables against invalid DRTM"
+NAME=test-0001.sh
+TMPLOG=$TMP/drtm.log.$$
+
+$FWTS --show-tests | grep drtm > /dev/null
+if [ $? -eq 1 ]; then
+	echo SKIP: $TEST, $NAME
+	exit 77
+fi
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/drtm-0001/acpidump-0002.log drtm - | cut -c7- | grep "^drtm" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/drtm-0001/drtm-0002.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+rm $TMPLOG
+exit $ret

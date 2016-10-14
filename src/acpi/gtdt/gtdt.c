93abe39d0ec0c4f16253e1b1dbe9b62441d0ea83
Jeffrey Hugo <jhugo@codeaurora.org>
2016-10-14 11:53:34 -0600

    acpi: gtdt: Account for added virtual timer flags
    ACPI v5.1 defined timer flag bit 2 as "Always-on".  Update the test to
    account for this.
    
    Signed-off-by: Jeffrey Hugo <jhugo@codeaurora.org>
    Acked-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>

--- src/acpi/gtdt/gtdt.c
+++ src/acpi/gtdt/gtdt.c
@@ -53,11 +53,11 @@ static int gtdt_test1(fwts_framework *fw)
 	uint32_t i = 0, n;
 	const fwts_acpi_table_gtdt *gtdt = (const fwts_acpi_table_gtdt *)table->data;
 
-	if (gtdt->virtual_timer_flags & ~3) {
+	if (gtdt->virtual_timer_flags & ~7) {
 		passed = false;
 		fwts_failed(fw, LOG_LEVEL_LOW,
 			"GTDTFlagReservedNonZero",
-			"GTDT flag reserved bits 2 to 31 are non-zero, "
+			"GTDT flag reserved bits 3 to 31 are non-zero, "
 			"instead got 0x%" PRIx32 ".", gtdt->virtual_timer_flags);
 	}
 

dca73bb3cabe56f68b8dc8ac313aaceb1c1c061b
Jeffrey Hugo <jhugo@codeaurora.org>
2016-10-14 13:17:17 -0600

    acpi: mpst: Show test as skipped if table does not exist
    If the table does not exist, the error message states the test is being
    skipped, however the test is categorized as aborted.  The majority of other
    tests are categorized as skipped under this scenario.  Update the init path
    to return a SKIP error to categorize the test result similar to other tests.
    
    Signed-off-by: Jeffrey Hugo <jhugo@codeaurora.org>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>

--- src/acpi/mpst/mpst.c
+++ src/acpi/mpst/mpst.c
@@ -33,7 +33,7 @@ static int mpst_init(fwts_framework *fw)
 	}
 	if (table == NULL) {
 		fwts_log_error(fw, "ACPI MPST table does not exist, skipping test");
-		return FWTS_ERROR;
+		return FWTS_SKIP;
 	}
 
 	return FWTS_OK;

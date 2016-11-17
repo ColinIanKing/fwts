48fa0dcc901456533818b348857faa4ea5b0584f
Colin Ian King <colin.king@canonical.com>
2016-11-17 18:13:51 +0000

    ACPICA: Update to version 20161117
    Changes in this release of ACPICA are detailed at the following
    link on the ACPICA developer mailing list:
    
        https://lists.acpica.org/pipermail/devel/2016-November/001058.html
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>

--- src/acpica/source/include/acpixf.h
+++ src/acpica/source/include/acpixf.h
@@ -118,7 +118,7 @@
 
 /* Current ACPICA subsystem version in YYYYMMDD format */
 
-#define ACPI_CA_VERSION                 0x20160930
+#define ACPI_CA_VERSION                 0x20161117
 
 #include "acconfig.h"
 #include "actypes.h"
@@ -653,6 +653,11 @@ AcpiGetTable (
     ACPI_STRING             Signature,
     UINT32                  Instance,
     ACPI_TABLE_HEADER       **OutTable))
+
+ACPI_EXTERNAL_RETURN_VOID (
+void
+AcpiPutTable (
+    ACPI_TABLE_HEADER       *Table))
 
 ACPI_EXTERNAL_RETURN_STATUS (
 ACPI_STATUS

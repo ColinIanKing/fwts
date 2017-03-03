4fa691febd0cd482aa30f9fa5a904f904d418a28
Colin Ian King <colin.king@canonical.com>
2017-03-03 18:03:30 +0000

    ACPICA: Update to version 20170303
    This pulls in the following ACPICA changes since the previous
    release:
    
    0) ACPICA licensing:
    
    The licensing information at the start of each source code module has
    been updated. In addition to the Intel license, the dual GPLv2/BSD
    license has been added for completeness. Now, a single version of the
    ource code should be suitable for all ACPICA customers. This is the
    major change for this release since it affects all source code modules.
    
    1) ACPICA kernel-resident subsystem:
    
    Fixed two issues with the common asltypes.h header that could cause
    problems in some environments: (Kim Jung-uk)
        Removed typedef for YY_BUFFER_STATE ?
           Fixes an error with earlier versions of Flex.
        Removed use of FILE typedef (which is only defined in stdio.h)
    
    2) iASL Compiler/Disassembler and Tools:
    
    Disassembler: fixed a regression introduced in 20170224. A fix for a
    memory leak related to resource descriptor tags (names) could fault when
    the disassembler was generated with 64-bit compilers.
    
    The ASLTS test suite has been updated to implement a new testing
    architecture. During generation of the suite from ASL source, both the
    ASL and ASL+ compilers are now validated, as well as the disassembler
    itself (Erik Schmauss). The architecture executes as follows:
    
        For every ASL source module:
            Compile (legacy ASL compilation)
            Disassemble the resulting AML to ASL+ source code
            Compile the new ASL+ module
            Perform a binary compare on the legacy AML and the new ASL+ AML
        The ASLTS suite then executes normally using the AML binaries.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>

--- src/acpica/source/common/adisasm.c
+++ src/acpica/source/common/adisasm.c
@@ -111,6 +111,42 @@
  * other governmental approval, or letter of assurance, without first obtaining
  * such license, approval or letter.
  *
+ *****************************************************************************
+ *
+ * Alternatively, you may choose to be licensed under the terms of the
+ * following license:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions, and the following disclaimer,
+ *    without modification.
+ * 2. Redistributions in binary form must reproduce at minimum a disclaimer
+ *    substantially similar to the "NO WARRANTY" disclaimer below
+ *    ("Disclaimer") and any redistribution must be conditioned upon
+ *    including a substantially similar Disclaimer requirement for further
+ *    binary redistribution.
+ * 3. Neither the names of the above-listed copyright holders nor the names
+ *    of any contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Alternatively, you may choose to be licensed under the terms of the
+ * GNU General Public License ("GPL") version 2 as published by the Free
+ * Software Foundation.
+ *
  *****************************************************************************/
 
 #include "aslcompiler.h"
@@ -194,24 +230,36 @@ AdInitialize (
     Status = AcpiOsInitialize ();
     if (ACPI_FAILURE (Status))
     {
+        fprintf (stderr, "Could not initialize ACPICA subsystem: %s\n",
+            AcpiFormatException (Status));
+
         return (Status);
     }
 
     Status = AcpiUtInitGlobals ();
     if (ACPI_FAILURE (Status))
     {
+        fprintf (stderr, "Could not initialize ACPICA globals: %s\n",
+            AcpiFormatException (Status));
+
         return (Status);
     }
 
     Status = AcpiUtMutexInitialize ();
     if (ACPI_FAILURE (Status))
     {
+        fprintf (stderr, "Could not initialize ACPICA mutex objects: %s\n",
+            AcpiFormatException (Status));
+
         return (Status);
     }
 
     Status = AcpiNsRootInitialize ();
     if (ACPI_FAILURE (Status))
     {
+        fprintf (stderr, "Could not initialize ACPICA namespace: %s\n",
+            AcpiFormatException (Status));
+
         return (Status);
     }
 
@@ -221,7 +269,7 @@ AdInitialize (
     AcpiGbl_RootTableList.CurrentTableCount = 0;
     AcpiGbl_RootTableList.Tables = LocalTables;
 
-    return (Status);
+    return (AE_OK);
 }
 
 

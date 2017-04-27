ColinIanKing/fwts

commit 5a4034ff7938d0e6ba3cccdff1f3cdf8f6bcd307

lib: fwts_gpe: free original gpe buffer on failed realloc
A common bug is where realloc fails to allocate and we assume that
the memory being realloc'd was freed. This is not the case, the
NULL return means we need to free the original gpe buffer to avoid
a memory leak.

Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/lib/src/fwts_gpe.c
@@ -62,12 +62,17 @@ int fwts_gpe_read(fwts_gpe **gpes)
 	while ((entry = readdir(dir)) != NULL) {
 		if ((strncmp(entry->d_name, "gpe", 3) == 0) ||
 		    (strncmp(entry->d_name, "sci",3) == 0)) {
-			if ((*gpes = realloc(*gpes, sizeof(fwts_gpe) * (n+1))) == NULL)
+			fwts_gpe *tmp;
+
+			tmp = realloc(*gpes, sizeof(fwts_gpe) * (n+1));
+			if (!tmp) {
+				free(*gpes);
 				goto error;
-			else {
+			} else {
 				char path[PATH_MAX];
 				char *data;
 
+				*gpes = tmp;
 				if (((*gpes)[n].name  = strdup(entry->d_name)) == NULL)
 					goto error;
 

commit 85c5c8da580555f6ce105e9b607845f5ce1334c8

lib: fwts_gpe: fix double free on gpe buffer
My previous memory leak fix introduced a double free bug, fix this by
not free'ing the buffer twice.  Issue detected by CoverityScan.

Fixes: 5a4034ff7938 ("lib: fwts_gpe: free original gpe buffer on failed realloc")

Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/lib/src/fwts_gpe.c
@@ -66,7 +66,6 @@ int fwts_gpe_read(fwts_gpe **gpes)
 
 			tmp = realloc(*gpes, sizeof(fwts_gpe) * (n+1));
 			if (!tmp) {
-				free(*gpes);
 				goto error;
 			} else {
 				char path[PATH_MAX];
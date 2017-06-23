a321a0376c9c3d80ab8216a7c470d0aed4df7608
Alex Hung <alex.hung@canonical.com>
2017-06-22 22:03:40 -0700

    fwts-test: add wsmt test
    Signed-off-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>

---
+++ fwts-test/wsmt-0001/test-0001.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+TEST="Test apcitables against WSMT"
+NAME=test-0001.sh
+TMPLOG=$TMP/wsmt.log.$$
+
+$FWTS --show-tests | grep wsmt > /dev/null
+if [ $? -eq 1 ]; then
+	echo SKIP: $TEST, $NAME
+	exit 77
+fi
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/wsmt-0001/acpidump-0001.log wsmt - | cut -c7- | grep "^wsmt" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/wsmt-0001/wsmt-0001.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+rm $TMPLOG
+exit $ret

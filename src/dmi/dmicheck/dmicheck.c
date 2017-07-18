ColinIanKing/fwts

commit 07bffc61ffb0452ed1d6e034bdd0a484849a1f63

dmi: dmicheck: ensure mmap'd SMBIOS data is readable before accessing it
We need to check we don't get SIGSEGV or SIGBUS errors when reading
the mmap'd SMBIOS data before we try and access it. Use the fwts_safe_memread
check on the data to sanity check these mappings.

Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/dmi/dmicheck/dmicheck.c
@@ -332,6 +332,12 @@ static void* dmi_table_smbios(fwts_framework *fw, fwts_smbios_entry *entry)
 
 	mem = fwts_mmap(addr, length);
 	if (mem != FWTS_MAP_FAILED) {
+		/* Can we safely copy the table? */
+		if (fwts_safe_memread((void *)addr, length) != FWTS_OK) {
+			fwts_log_info(fw, "SMBIOS table at %p cannot be read", (void *)addr);
+			(void)fwts_munmap(mem, length);
+			return NULL;
+		}
 		table = malloc(length);
 		if (table)
 			memcpy(table, mem, length);
@@ -373,6 +379,12 @@ static void* dmi_table_smbios30(fwts_framework *fw, fwts_smbios30_entry *entry)
 
 	mem = fwts_mmap(addr, length);
 	if (mem != FWTS_MAP_FAILED) {
+		/* Can we safely copy the table? */
+		if (fwts_safe_memread((void *)addr, length) != FWTS_OK) {
+			fwts_log_info(fw, "SMBIOS table at %p cannot be read", (void *)addr);
+			(void)fwts_munmap(mem, length);
+			return NULL;
+		}
 		table = malloc(length);
 		if (table)
 			fwts_memcpy_unaligned(table, mem, length);

commit 4efd191692401f39a3b7254b6b36ac24cbf76ce7

dmi: dmicheck: fix incorrect safe memory read check on DMI mmap'd region
We should be checking the mmap'd address and not the original physical address.

Fixes: 07bffc61ffb0 ("dmi: dmicheck: ensure mmap'd SMBIOS data is readable before accessing it")
Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/dmi/dmicheck/dmicheck.c
@@ -333,7 +333,7 @@ static void* dmi_table_smbios(fwts_framework *fw, fwts_smbios_entry *entry)
 	mem = fwts_mmap(addr, length);
 	if (mem != FWTS_MAP_FAILED) {
 		/* Can we safely copy the table? */
-		if (fwts_safe_memread((void *)addr, length) != FWTS_OK) {
+		if (fwts_safe_memread((void *)mem, length) != FWTS_OK) {
 			fwts_log_info(fw, "SMBIOS table at %p cannot be read", (void *)addr);
 			(void)fwts_munmap(mem, length);
 			return NULL;
@@ -380,7 +380,7 @@ static void* dmi_table_smbios30(fwts_framework *fw, fwts_smbios30_entry *entry)
 	mem = fwts_mmap(addr, length);
 	if (mem != FWTS_MAP_FAILED) {
 		/* Can we safely copy the table? */
-		if (fwts_safe_memread((void *)addr, length) != FWTS_OK) {
+		if (fwts_safe_memread((void *)mem, length) != FWTS_OK) {
 			fwts_log_info(fw, "SMBIOS table at %p cannot be read", (void *)addr);
 			(void)fwts_munmap(mem, length);
 			return NULL;
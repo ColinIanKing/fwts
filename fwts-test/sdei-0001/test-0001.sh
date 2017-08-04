76a99d2b1de5fd97ba705fc2ebc7085c06e5c60e
Alex Hung <alex.hung@canonical.com>
2017-08-04 15:49:56 -0700

    fwts-test: Add regression test for SDEI
    Signed-off-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>

---
+++ fwts-test/sdei-0001/test-0001.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+TEST="Test acpitables against SDEI"
+NAME=test-0001.sh
+TMPLOG=$TMP/sdei.log.$$
+
+$FWTS --show-tests | grep sdei > /dev/null
+if [ $? -eq 1 ]; then
+	echo SKIP: $TEST, $NAME
+	exit 77
+fi
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/sdei-0001/acpidump-0001.log sdei - | cut -c7- | grep "^sdei" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/sdei-0001/sdei-0001.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+rm $TMPLOG
+exit $ret

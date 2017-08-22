ab55576ef9095aaccad4156d72e2295d69044e30
Alex Hung <alex.hung@canonical.com>
2017-08-22 12:30:06 -0700

    fwts-test: Add regression test for RASF
    Signed-off-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>

---
+++ fwts-test/rasf-0001/test-0001.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+TEST="Test acpitables against RASF"
+NAME=test-0001.sh
+TMPLOG=$TMP/rasf.log.$$
+
+$FWTS --show-tests | grep rasf > /dev/null
+if [ $? -eq 1 ]; then
+	echo SKIP: $TEST, $NAME
+	exit 77
+fi
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/rasf-0001/acpidump-0001.log rasf - | cut -c7- | grep "^rasf" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/rasf-0001/rasf-0001.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+rm $TMPLOG
+exit $ret

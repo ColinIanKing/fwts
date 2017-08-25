ColinIanKing/fwts

commit fcc5f1e9fbc73dc7576d871f910a254b6b652468

acpi: xsdt: add SBBR compliance tests
Server Base Boot Requirements (SBBR) specification is intended for SBSA-
compliant 64-bit ARMv8 servers. It defines the base firmware
requirements for out-of-box support of any ARM SBSA-compatible
Operating System or hypervisor. The requirements in this specification
are expected to be minimal yet complete for booting a multi-core ARMv8
server platform, while leaving plenty of room for OEM or ODM innovations
and design details. For more information, download the SBBR
specification here:
http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.den0044b/index.html

This change introduces test cases as per SBBR specification to xsdt acpi table.

Signed-off-by: Supreeth Venkatesh <supreeth.venkatesh@arm.com>
Signed-off-by: Mahesh Bireddy <mahesh.reddybireddy@arm.com>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Colin Ian King <colin.king@canonical.com>

src/acpi/xsdt/xsdt.c
@@ -37,8 +37,13 @@ static int xsdt_init(fwts_framework *fw)
 		return FWTS_ERROR;
 	}
 	if (table == NULL || (table && table->length == 0)) {
-		fwts_log_error(fw, "ACPI XSDT table does not exist, skipping test");
-		return FWTS_SKIP;
+		if (fw->flags & FWTS_FLAG_TEST_SBBR) {
+			fwts_log_error(fw, "ACPI XSDT table does not exist");
+			return FWTS_ERROR;
+		} else {
+			fwts_log_error(fw, "ACPI XSDT table does not exist, skipping test");
+			return FWTS_SKIP;
+		}
 	}
 	return FWTS_OK;
 }
@@ -56,9 +61,16 @@ static int xsdt_test1(fwts_framework *fw)
 	for (i = 0; i < n; i++) {
 		if (xsdt->entries[i] == 0) {
 			passed = false;
-			fwts_failed(fw, LOG_LEVEL_MEDIUM,
-				"XSDTEntryNull",
-				"XSDT Entry %zd is null, should not be non-zero.", i);
+			if (fw->flags & FWTS_FLAG_TEST_SBBR) {
+				fwts_failed(fw, LOG_LEVEL_CRITICAL,
+					"XSDTEntryNull",
+					"XSDT Entry %zd is null, should not be non-zero.", i);
+			}
+			else {
+				fwts_failed(fw, LOG_LEVEL_MEDIUM,
+					"XSDTEntryNull",
+					"XSDT Entry %zd is null, should not be non-zero.", i);
+			}
 			fwts_advice(fw,
 				"A XSDT pointer is null and therefore erroneously "
 				"points to an invalid 64 bit ACPI table header. "
@@ -67,8 +79,15 @@ static int xsdt_test1(fwts_framework *fw)
 				"should be fixed where possible.");
 		}
 	}
-	if (passed)
-		fwts_passed(fw, "No issues found in XSDT table.");
+	if (passed) {
+		if (fw->flags & FWTS_FLAG_TEST_SBBR) {
+			fwts_passed(fw, "XSDT is present, pointed at by XsdrAddress=0x%lx"
+				" and contain valid pointers to %d other ACPI tables mandated by SBBR",
+				 xsdt->entries[0], (int)n);
+		}
+		else
+			fwts_passed(fw, "No issues found in XSDT table.");
+	}
 
 	return FWTS_OK;
 }
@@ -84,6 +103,6 @@ static fwts_framework_ops xsdt_ops = {
 	.minor_tests = xsdt_tests
 };
 
-FWTS_REGISTER("xsdt", &xsdt_ops, FWTS_TEST_ANYTIME, FWTS_FLAG_BATCH | FWTS_FLAG_TEST_ACPI)
+FWTS_REGISTER("xsdt", &xsdt_ops, FWTS_TEST_ANYTIME, FWTS_FLAG_BATCH | FWTS_FLAG_TEST_ACPI | FWTS_FLAG_TEST_SBBR)
 
 #endif

commit 1975e0f461333dc8b76398720632391cf5f0e842

acpi: xsdt: fix build error on 32 bit systems
Use PRIu64 rather than lx to print the address. Also minor change
to else formatting.

Fixes build error:
acpi/xsdt/xsdt.c: In function ‘xsdt_test1’:
acpi/xsdt/xsdt.c:84:4: error: format ‘%lx’ expects argument of type
‘long unsigned int’, but argument 7 has type ‘uint64_t’ [-Werror=format=]
    fwts_passed(fw, "XSDT is present, pointed at by XsdrAddress=0x%lx"
    ^

Fixes: fcc5f1e9fbc7 ("acpi: xsdt: add SBBR compliance tests)
Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/acpi/xsdt/xsdt.c
@@ -81,11 +81,10 @@ static int xsdt_test1(fwts_framework *fw)
 	}
 	if (passed) {
 		if (fw->flags & FWTS_FLAG_TEST_SBBR) {
-			fwts_passed(fw, "XSDT is present, pointed at by XsdrAddress=0x%lx"
+			fwts_passed(fw, "XSDT is present, pointed at by XsdrAddress=0x%" PRIx64
 				" and contain valid pointers to %d other ACPI tables mandated by SBBR",
 				 xsdt->entries[0], (int)n);
-		}
-		else
+		} else
 			fwts_passed(fw, "No issues found in XSDT table.");
 	}
 
a317ed0dcc3258b1a860e6ae70e8435f21b98759
Colin Ian King <colin.king@canonical.com>
2017-09-04 14:16:01 +0100

    uefi: uefirttime: constify static array dayofmonth
    The static and read-only array can also be made const
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>

--- src/uefi/uefirttime/uefirttime.c
+++ src/uefi/uefirttime/uefirttime.c
@@ -36,7 +36,7 @@
 		((year) % 4 == 0 && ((year) % 100 != 0 || (year) % 400 == 0))
 
 static int fd;
-static uint32_t dayofmonth[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+static const uint32_t dayofmonth[12] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 static bool dayvalid(EFI_TIME *Time)
 {

ColinIanKing/fwts

commit c576305f7b29ed17b71328ff432d8377f32c6d3a

fwts_log: make line_number to a uint32_t type
Line numbers are never negative, so make line_number a uint32_t type

Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/lib/src/fwts_log_xml.c
@@ -20,6 +20,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdint.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
@@ -67,7 +68,7 @@ static int fwts_log_print_xml(
 
 	fprintf(log_file->fp, "%*s<logentry>\n", xml_stack_index * XML_INDENT, "");
 
-	fprintf(log_file->fp, "%*s<line_num>%d</line_num>\n",
+	fprintf(log_file->fp, "%*s<line_num>%" PRIu32 "</line_num>\n",
 		(xml_stack_index + 1) * XML_INDENT,
 		"", log_file->line_number);
 

src/lib/include/fwts_log.h
@@ -97,7 +97,7 @@ typedef struct log_t {
  */
 typedef struct {
 	FILE *fp;				/* file descriptor for log */
-	int line_number;			/* keeps track of the line numbering */
+	uint32_t line_number;			/* keeps track of the line numbering */
 	fwts_log *log;				/* parent log struct */
 	fwts_log_type type;			/* log type */
 	fwts_log_filename_type filename_type;	/* log filename type */

src/lib/src/fwts_log_plaintext.c
@@ -21,6 +21,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdint.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
@@ -52,7 +53,7 @@ static int fwts_log_header_plaintext(
 			ptr++;
 			if (!strncmp(ptr, "line", 4)) {
 				n += snprintf(buffer + n, len - n,
-					"%5.5d", log_file->line_number);
+					"%5.5" PRIu32, log_file->line_number);
 				ptr += 4;
 			}
 			if (!strncmp(ptr, "date", 4)) {

commit 2ddc763f269ac34473ef4104e16c0046bf9a12b8

lib: fwts_log_xml: include inttypes.h for PRIu32 definition
Older versions of gcc/glibc require this header to be explicitly included.

Fixes: c576305f7b29 ("fwts_log: make line_number to a uint32_t type")
Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/lib/src/fwts_log_xml.c
@@ -21,6 +21,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
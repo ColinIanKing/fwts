ColinIanKing/fwts

commit d5ae8c039c50cd1cf9a82cf0fd9d55858dc29cbc

lib: fwts_wakealarm: make wakealarm times uint32_t
A wakealarm of negative time should not be allowed, so make the
timings uint32_t rather than int.

Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/acpi/wakealarm/wakealarm.c
@@ -92,11 +92,11 @@ static int wakealarm_test3(fwts_framework *fw)
 
 static int wakealarm_test4(fwts_framework *fw)
 {
-	int i;
+	uint32_t i;
 	int failed = 0;
 
 	for (i = 1; i < 5; i++) {
-		fwts_log_info(fw, "Trigger wakealarm for %d seconds in the future.", i);
+		fwts_log_info(fw, "Trigger wakealarm for %" PRIu32 " seconds in the future.", i);
 		int ret = fwts_wakealarm_test_firing(fw, i);
 		if (ret < 0) {
 			fwts_failed(fw, LOG_LEVEL_MEDIUM,

src/lib/include/fwts_wakealarm.h
@@ -25,8 +25,8 @@
 #include "fwts_framework.h"
 
 int fwts_wakealarm_exits(fwts_framework *fw);
-int fwts_wakealarm_test_firing(fwts_framework *fw, const int sleep);
-int fwts_wakealarm_trigger(fwts_framework *fw, const int seconds);
+int fwts_wakealarm_test_firing(fwts_framework *fw, const uint32_t sleep);
+int fwts_wakealarm_trigger(fwts_framework *fw, const uint32_t seconds);
 int fwts_wakealarm_cancel(fwts_framework *fw);
 int fwts_wakealarm_get(fwts_framework *fw, struct rtc_time *rtc_tm);
 int fwts_wakealarm_set(fwts_framework *fw, struct rtc_time *rtc_tm);

src/lib/src/fwts_wakealarm.c
@@ -93,7 +93,7 @@ int fwts_wakealarm_exits(fwts_framework *fw)
  *  fwts_wakealarm_trigger()
  *	trigger the RTC wakealarm to fire in 'seconds' seconds from now.
  */
-int fwts_wakealarm_trigger(fwts_framework *fw, const int seconds)
+int fwts_wakealarm_trigger(fwts_framework *fw, const uint32_t seconds)
 {
 	int fd, ret = FWTS_OK;
 	struct rtc_time rtc_tm;
@@ -161,7 +161,7 @@ int fwts_wakealarm_cancel(fwts_framework *fw)
  *  fwts_wakealarm_check_fired()
  *	check if wakealarm fires
  */
-int fwts_wakealarm_check_fired(fwts_framework *fw, const int seconds)
+int fwts_wakealarm_check_fired(fwts_framework *fw, const uint32_t seconds)
 {
 	int fd, rc, ret = FWTS_OK;
 	fd_set rfds;
@@ -204,7 +204,7 @@ out:
  *	test RTC wakealarm trigger and firing from 'seconds' seconds time
  * 	from now.  returns FWTS_OK if passed, otherwise FWTS_ERROR.
  */
-int fwts_wakealarm_test_firing(fwts_framework *fw, const int seconds)
+int fwts_wakealarm_test_firing(fwts_framework *fw, const uint32_t seconds)
 {
 	int ret = FWTS_OK;
 

commit 322238be4258f0f148dcdd23a31c3e383da16e39

acpi: wakealarm: include stdint.h and inttypes.h
Older versions of gcc/glibc require these headers to be explicitly
included.

Fixes: d5ae8c039c50 ("lib: fwts_wakealarm: make wakealarm times uint32_t")
Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/acpi/wakealarm/wakealarm.c
@@ -22,6 +22,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
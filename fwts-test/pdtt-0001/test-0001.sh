0dee6c5a78822b2551c001e7eb042a26b10539e9
Alex Hung <alex.hung@canonical.com>
2017-10-20 22:44:47 -0700

    fwts-test: Add regression test for PDTT
    Signed-off-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>

---
+++ fwts-test/pdtt-0001/test-0001.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+TEST="Test acpitables against PDTT"
+NAME=test-0001.sh
+TMPLOG=$TMP/pdtt.log.$$
+
+$FWTS --show-tests | grep pdtt > /dev/null
+if [ $? -eq 1 ]; then
+	echo SKIP: $TEST, $NAME
+	exit 77
+fi
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/pdtt-0001/acpidump-0001.log pdtt - | cut -c7- | grep "^pdtt" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/pdtt-0001/pdtt-0001.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+rm $TMPLOG
+exit $ret

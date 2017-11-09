ColinIanKing/fwts

commit ee769ccf294cff2c63769482e84866a979d42f44

hpet: fix the false alarm of hpet configuration test
Some platforms got the report,
Invalid Vendor ID: ffff - this should be configured.
Invalid clock period 4294967295, must be non-zero and less than 10^8.
after the commit#7cfe1a41956294bd8062b7680e1778ed9ee147b2 applied.

It is because HPET spec defined those registers need to 32-bit or 64-bit
accesses. 8-bit read causes the following checking fail.

Signed-off-by: Ivan Hu <ivan.hu@canonical.com>
Acked-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>

src/lib/src/fwts_safe_mem.c
@@ -92,3 +92,30 @@ int OPTIMIZE0 fwts_safe_memread(const void *src, const size_t n)
 
 	return FWTS_OK;
 }
+
+int OPTIMIZE0 fwts_safe_memread32(const void *src, const size_t n)
+{
+	static uint32_t buffer[256];
+	const uint32_t *ptr, *end = src + n;
+	uint32_t *bufptr;
+	const uint32_t *bufend = buffer + sizeof(buffer);
+
+	if (sigsetjmp(jmpbuf, 1) != 0)
+		return FWTS_ERROR;
+
+	fwts_sig_handler_set(SIGSEGV, sig_handler, &old_segv_action);
+	fwts_sig_handler_set(SIGBUS, sig_handler, &old_bus_action);
+
+	for (bufptr = buffer, ptr = src; ptr < end; ptr++) {
+		/* Force data to be read */
+		__builtin_prefetch(ptr, 0, 3);
+		*bufptr = *ptr;
+		bufptr++;
+		bufptr = (bufptr >= bufend) ? buffer : bufptr;
+	}
+
+	fwts_sig_handler_restore(SIGSEGV, &old_segv_action);
+	fwts_sig_handler_restore(SIGBUS, &old_bus_action);
+
+	return FWTS_OK;
+}

src/acpi/hpet/hpet.c
@@ -428,7 +428,7 @@ static int hpet_check_test4(fwts_framework *fw)
 		return FWTS_ERROR;
 	}
 
-	if (fwts_safe_memread(hpet_base_v, HPET_REG_SIZE) != FWTS_OK) {
+	if (fwts_safe_memread32(hpet_base_v, HPET_REG_SIZE / 4) != FWTS_OK) {
 		fwts_log_info(fw, "Test skipped because HPET region cannot be read.");
 		(void)fwts_munmap(hpet_base_v, HPET_REG_SIZE);
 		return FWTS_SKIP;

src/lib/include/fwts_safe_mem.h
@@ -22,5 +22,6 @@
 
 int fwts_safe_memcpy(void *dst, const void *src, const size_t n);
 int fwts_safe_memread(const void *src, const size_t n);
+int fwts_safe_memread32(const void *src, const size_t n);
 
 #endif

commit d77356b27858ef7291c5ea3784efb45410a27bb4

lib: fwts_safe_mem: fix buffer end calculation
The calculation of the end of the buffer needs to take into account
the size of the elements in the buffer; divide by the element size
to get the correct size.

Detected by CoverityScan, CID#1382558 ("Incorrect expression") and
CID#1382559 ("Memory Corruptions")

Fixes: ee769ccf294c ("hpet: fix the false alarm of hpet configuration test")
Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Anthony Wong <anthony.wong@canonical.com>

src/lib/src/fwts_safe_mem.c
@@ -93,12 +93,20 @@ int OPTIMIZE0 fwts_safe_memread(const void *src, const size_t n)
 	return FWTS_OK;
 }
 
+/*
+ *  fwts_safe_memread()
+ *	check we can safely read a region of memory. This catches
+ *	SIGSEGV/SIGBUS errors and returns FWTS_ERROR if it is not
+ *	readable or FWTS_OK if it's OK.
+ *
+ *	n = number of of 32 bit words to check
+ */
 int OPTIMIZE0 fwts_safe_memread32(const void *src, const size_t n)
 {
 	static uint32_t buffer[256];
 	const uint32_t *ptr, *end = src + n;
 	uint32_t *bufptr;
-	const uint32_t *bufend = buffer + sizeof(buffer);
+	const uint32_t *bufend = buffer + (sizeof(buffer) / sizeof(*buffer));
 
 	if (sigsetjmp(jmpbuf, 1) != 0)
 		return FWTS_ERROR;
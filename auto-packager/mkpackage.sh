44642b8a0672b1d995a3ee2354fa5cd3329913d3
Ivan Hu <ivan.hu@canonical.com>
2018-02-01 17:23:09 +0800

    auto-packager: mkpackage.sh: remove zesty
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Alex Hung <alex.hung@canonical.com>

--- auto-packager/mkpackage.sh
+++ auto-packager/mkpackage.sh
@@ -23,7 +23,7 @@
 # Get fwts sources, strip out .git directory, add in necessary debian packaging
 # files, build source package ready for upload.
 #
-RELEASES="trusty xenial zesty artful bionic"
+RELEASES="trusty xenial artful bionic"
 REPO=git://kernel.ubuntu.com/hwe/fwts.git
 RELEASE_TAR_URL=http://fwts.ubuntu.com/release
 FWTS=fwts

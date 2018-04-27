ColinIanKing/fwts

commit 0eb85f83c2600565191688620396dbb973386a1c

fan: cur_state == -1 is valid for intel_powerclamp
In Documentation/thermal/intel_powerclamp.txt, it states that the
"When idle injection is disabled, reading cur_state returns value -1
instead of 0 which is to avoid confusing 100% busy state with the
disabled state".

Signed-off-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/acpi/fan/fan.c
@@ -154,7 +154,7 @@ static int fan_test1(fwts_framework *fw)
 			fwts_failed(fw, LOG_LEVEL_MEDIUM, "NoFanMaxState",
 				"Fan %s present but has no max_state present.",
 				info->name);
-		if (info->cur_state == -1)
+		if (info->cur_state == -1 && strcmp(info->type, "intel_powerclamp"))
 			fwts_failed(fw, LOG_LEVEL_MEDIUM, "NoFanCurState",
 				"Fan %s present but has no cur_state present.",
 				info->name);

commit 0bec57ae9f1ce064da0edad7b72e9122feac0363

acpi: fan: fix potential null pointer dereference issue
Check for non-null info->type before dereferencing it with a strcmp.

Detected by CoverityScan, CID#1390651 ("Null pointer dereferences")

Fixes: 0eb85f83c260 ("fan: cur_state == -1 is valid for intel_powerclamp")
Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/acpi/fan/fan.c
@@ -154,16 +154,19 @@ static int fan_test1(fwts_framework *fw)
 			fwts_failed(fw, LOG_LEVEL_MEDIUM, "NoFanMaxState",
 				"Fan %s present but has no max_state present.",
 				info->name);
-		if (info->cur_state == -1 && strcmp(info->type, "intel_powerclamp"))
-			fwts_failed(fw, LOG_LEVEL_MEDIUM, "NoFanCurState",
-				"Fan %s present but has no cur_state present.",
-				info->name);
+		if (info->type) {
+			if ((info->cur_state == -1) &&
+			    strcmp(info->type, "intel_powerclamp"))
+				fwts_failed(fw, LOG_LEVEL_MEDIUM, "NoFanCurState",
+					"Fan %s present but has no cur_state present.",
+					info->name);
 
-		if (info->type &&
-		    (info->max_state >= 0) && (info->cur_state >= 0))
-			fwts_passed(fw, "Fan %s of type %s has max cooling state %d "
-				"and current cooling state %d.",
-					info->name, info->type, info->max_state, info->cur_state);
+			if ((info->max_state >= 0) && (info->cur_state >= 0))
+				fwts_passed(fw, "Fan %s of type %s has max cooling "
+					"state %d and current cooling state %d.",
+					info->name, info->type, info->max_state,
+					info->cur_state);
+		}
 	}
 
 	fwts_list_free(fans, free_fan_info);
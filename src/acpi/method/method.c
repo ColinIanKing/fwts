ColinIanKing/fwts

commit e2a21f8d75d82179f3c535e2fc3454907aa2e77c

acpi: method: skip scope names in method_evaluate_method
Signed-off-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/acpi/method/method.c
@@ -501,14 +501,31 @@ static int method_evaluate_method(fwts_framework *fw,
 	size_t name_len = strlen(name);
 	bool found = false;
 
+
 	if ((methods = fwts_acpi_object_get_names()) != NULL) {
 		fwts_list_link	*item;
 
 		fwts_list_foreach(item, methods) {
 			char *method_name = fwts_list_data(char*, item);
+			ACPI_HANDLE method_handle;
+			ACPI_OBJECT_TYPE type;
+			ACPI_STATUS status;
+
 			size_t len = strlen(method_name);
 			if (strncmp(name, method_name + len - name_len, name_len) == 0) {
 				ACPI_OBJECT_LIST  arg_list;
+
+				status = AcpiGetHandle (NULL, method_name, &method_handle);
+				if (ACPI_FAILURE(status)) {
+					fwts_warning(fw, "Failed to get handle for object %s.", name);
+				}
+				status = AcpiGetType(method_handle, &type);
+				if (ACPI_FAILURE(status)) {
+					fwts_warning(fw, "Failed to get object type for %s.",name);
+				}
+
+				if (type == ACPI_TYPE_LOCAL_SCOPE)
+					continue;
 
 				found = true;
 				arg_list.Count   = num_args;

commit 205df285c9a809d0154451d4cf20c281bb4c9961

acpi: method: skip over bad method_handle and failed message type
When fetching the method_handle and the message type there are
issues warning about failures but no real error handling. This
can lead to an uninitialized method_handle being passed into
AcpiGetType that can cause an uninitialized pointer read. Fix
these failures by continuing on to the next method inthe list.

Addresses-Coverity: ("Uninitialized pointer read")
Fixes: e2a21f8d75d8 ("acpi: method: skip scope names in method_evaluate_method")
Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/acpi/method/method.c
@@ -410,10 +410,12 @@ static int method_evaluate_method(fwts_framework *fw,
 				status = AcpiGetHandle (NULL, method_name, &method_handle);
 				if (ACPI_FAILURE(status)) {
 					fwts_warning(fw, "Failed to get handle for object %s.", name);
+					continue;
 				}
 				status = AcpiGetType(method_handle, &type);
 				if (ACPI_FAILURE(status)) {
 					fwts_warning(fw, "Failed to get object type for %s.",name);
+					continue;
 				}
 
 				if (type == ACPI_TYPE_LOCAL_SCOPE)
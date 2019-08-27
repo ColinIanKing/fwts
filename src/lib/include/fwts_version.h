881f68c7c26bbf24d1f27a00a76458761d66a839
Ivan Hu <ivan.hu@canonical.com>
2019-08-27 13:02:41 +0800

    lib: fwts_version.h - update to V19.08.00
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

--- src/lib/include/fwts_version.h
+++ src/lib/include/fwts_version.h
@@ -16,5 +16,5 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  */
-#define FWTS_VERSION "V19.07.00"
-#define FWTS_DATE    "2019-07-25 16:49:38"
+#define FWTS_VERSION "V19.08.00"
+#define FWTS_DATE    "2019-08-27 05:02:41"

6d2238f764c6358a702ce530ed0eb1d645f6cc6f
Alex Hung <alex.hung@canonical.com>
2020-03-23 12:42:42 -0600

    lib: fwts_version.h - update to V20.03.00
    Signed-off-by: Alex Hung <alex.hung@canonical.com>

--- src/lib/include/fwts_version.h
+++ src/lib/include/fwts_version.h
@@ -16,5 +16,5 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  */
-#define FWTS_VERSION "V20.02.00"
-#define FWTS_DATE    "2020-02-26 01:41:33"
+#define FWTS_VERSION "V20.03.00"
+#define FWTS_DATE    "2020-03-23 18:42:42"

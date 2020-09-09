be365d3461196ae7dc10d570715bed004690bbcf
Alex Hung <alex.hung@canonical.com>
2020-09-09 17:17:01 -0600

    fwts-test: sync up with new s0idle tests
    Signed-off-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>

---
+++ fwts-test/s0idle-0001/test-0001.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+TEST="Test table against S0IDLE"
+NAME=test-0001.sh
+TMPLOG=$TMP/s0idle.log.$$
+
+$FWTS --show-tests | grep S0IDLE > /dev/null
+if [ $? -eq 1 ]; then
+	echo SKIP: $TEST, $NAME
+	exit 77
+fi
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/s0idle-0001/acpidump-0001.log s0idle - | cut -c7- | grep "^s0idle" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/s0idle-0001/s0idle-0001.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+rm $TMPLOG
+exit $ret

deb086de0b7b73dcad7358a4a56c900e75e7539c
Ivan Hu <ivan.hu@canonical.com>
2020-11-25 17:22:52 +0800

    lib: fwts_version.h - update to V20.11.00
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

--- src/lib/include/fwts_version.h
+++ src/lib/include/fwts_version.h
@@ -16,5 +16,5 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  */
-#define FWTS_VERSION "V20.09.00"
-#define FWTS_DATE    "2020-09-17 18:08:13"
+#define FWTS_VERSION "V20.11.00"
+#define FWTS_DATE    "2020-11-25 09:22:52"

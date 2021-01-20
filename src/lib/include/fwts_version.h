2ab9a9f076b6373f79d7a755b07d9e0e5d3e1c59
Alex Hung <alex.hung@canonical.com>
2021-01-20 01:31:44 -0700

    lib: fwts_version.h - update to V21.01.00
    Signed-off-by: Alex Hung <alex.hung@canonical.com>

--- src/lib/include/fwts_version.h
+++ src/lib/include/fwts_version.h
@@ -16,5 +16,5 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  */
-#define FWTS_VERSION "V20.12.00"
-#define FWTS_DATE    "2020-12-17 02:15:43"
+#define FWTS_VERSION "V21.01.00"
+#define FWTS_DATE    "2021-01-20 08:31:44"

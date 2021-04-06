ColinIanKing/fwts

commit 4ad3e374bf98931c84adcf6e523982b5ebb83748

acpi: fadt: X_GPE0/1_BLK can be zero when not used
ACPI spec states "if this register block is not supported, this field
contains zero".

This patch also replaces spaces by a tab for fwts_acpi_gas struct.

Signed-off-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/lib/src/fwts_acpi.c
@@ -59,4 +59,15 @@ void fwts_acpi_table_get_header(fwts_acpi_table_header *hdr, uint8_t *data)
 	memcpy(hdr, data, sizeof(fwts_acpi_table_header));
 }
 
+bool fwts_acpi_data_zero(const void *data, const size_t len) {
+	uint8_t *ptr = (uint8_t *) data;
+	uint8_t i;
+
+	for (i = 0; i < len; i++)
+		if (*ptr++)
+			return false;
+
+	return true;
+}
+
 #endif

src/acpi/fadt/fadt.c
@@ -1527,21 +1527,23 @@ static void acpi_table_check_fadt_x_gpex_blk(fwts_framework *fw) {
 	if (fadt->x_gpe0_blk.access_width == 1)
 		fwts_passed(fw, "FADT X_GPE0_BLK has correct byte access width.");
 	else {
-		fwts_failed(fw, LOG_LEVEL_HIGH,
-			"X_GPE0_BLKBadAccessWidth",
-			"FADT X_GPE0_BLK Access width 0x%2.2" PRIx8
-			" but it should be 1 (byte access).",
-			fadt->x_gpe0_blk.access_width);
+		if (!fwts_acpi_data_zero((const void *) &fadt->x_gpe0_blk, sizeof(fwts_acpi_gas)))
+			fwts_failed(fw, LOG_LEVEL_HIGH,
+				"X_GPE0_BLKBadAccessWidth",
+				"FADT X_GPE0_BLK Access width 0x%2.2" PRIx8
+				" but it should be 1 (byte access).",
+				fadt->x_gpe0_blk.access_width);
 	}
 
 	if (fadt->x_gpe1_blk.access_width == 1)
 		fwts_passed(fw, "FADT X_GPE1_BLK has correct byte access width.");
 	else {
-		fwts_failed(fw, LOG_LEVEL_HIGH,
-			"X_GPE1_BLKBadAccessWidth",
-			"FADT X_GPE1_BLK Access width 0x%2.2" PRIx8
-			" but it should be 1 (byte access).",
-			fadt->x_gpe1_blk.access_width);
+		if (!fwts_acpi_data_zero((const void *) &fadt->x_gpe1_blk, sizeof(fwts_acpi_gas)))
+			fwts_failed(fw, LOG_LEVEL_HIGH,
+				"X_GPE1_BLKBadAccessWidth",
+				"FADT X_GPE1_BLK Access width 0x%2.2" PRIx8
+				" but it should be 1 (byte access).",
+				fadt->x_gpe1_blk.access_width);
 	}
 }
 

src/lib/include/fwts_acpi.h
@@ -115,9 +115,9 @@ const char *fwts_acpi_fadt_preferred_pm_profile(const int profile);
 typedef struct {
 	uint8_t 	address_space_id;
 	uint8_t		register_bit_width;
-        uint8_t 	register_bit_offset;
-        uint8_t 	access_width;
-        uint64_t 	address;
+	uint8_t 	register_bit_offset;
+	uint8_t 	access_width;
+	uint64_t 	address;
 } __attribute__ ((packed)) fwts_acpi_gas;
 
 /*
@@ -1828,6 +1828,7 @@ typedef struct {
 } __attribute__ ((packed)) fwts_acpi_table_hest_generic_hardware_error_source_v2;
 
 void fwts_acpi_table_get_header(fwts_acpi_table_header *hdr, uint8_t *data);
+bool fwts_acpi_data_zero(const void *data, const size_t len);
 
 /*
  * ACPI CSTR (Core System Resources Table)

commit 0c0b84b4fb11a3d31adbe63350aa045015348f32

lib: fwts_acpi: fix potential infinite loop in data copy
The loop index i is a uint8_t however it is being compared to an
upper loop limit that is a wider type of size_t. When i reaches
255 it will wrap around to 0 and the loop will never exit. Fix this
by making i a size_t type.

Addresses-Coverity: ("Infinite loop")
Fixes: 4ad3e374bf98 ("acpi: fadt: X_GPE0/1_BLK can be zero when not used")
Signed-off-by: Colin Ian King <colin.king@canonical.com>
Acked-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/lib/src/fwts_acpi.c
@@ -59,9 +59,10 @@ void fwts_acpi_table_get_header(fwts_acpi_table_header *hdr, uint8_t *data)
 	memcpy(hdr, data, sizeof(fwts_acpi_table_header));
 }
 
-bool fwts_acpi_data_zero(const void *data, const size_t len) {
+bool fwts_acpi_data_zero(const void *data, const size_t len)
+{
 	uint8_t *ptr = (uint8_t *) data;
-	uint8_t i;
+	size_t i;
 
 	for (i = 0; i < len; i++)
 		if (*ptr++)
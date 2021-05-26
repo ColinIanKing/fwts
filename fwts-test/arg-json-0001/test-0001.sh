abf551463c3362977f536a4f0becc6eb1b94cdaf
Alex Hung <alex.hung@canonical.com>
2021-05-26 13:03:50 -0600

    fwts_test: arg-json-0001: use current klog.json, not a copy
    The testcase is using an old copy of the klog.json datafile
    and is out of date. Ensure the json file is the recent one in
    the data directory, remove the old copy and re-sync the test
    results.
    
    Signed-off-by: Colin Ian King <colin.king@canonical.com>
    Signed-off-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>

--- fwts-test/arg-json-0001/test-0001.sh
+++ fwts-test/arg-json-0001/test-0001.sh
@@ -3,12 +3,12 @@
 TEST="Test --json-data-path option"
 NAME=test-0001.sh
 TMPLOG=$TMP/klog.log.$$
-HERE=$FWTSTESTDIR/arg-json-0001
+HERE=$FWTSTESTDIR/../data
 
 $FWTS --log-format="%line %owner " -w 80 --json-data-path=$HERE --klog=$FWTSTESTDIR/arg-json-0001/klog.txt klog - | grep "^[0-9]*[ ]*klog" | cut -c7- > $TMPLOG
 diff $TMPLOG $FWTSTESTDIR/arg-json-0001/klog-0001.log >> $FAILURE_LOG
 ret=$?
-if [ $ret -eq 0 ]; then 
+if [ $ret -eq 0 ]; then
 	echo PASSED: $TEST, $NAME
 else
 	echo FAILED: $TEST, $NAME

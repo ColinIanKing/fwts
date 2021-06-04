94f2bbeae63187e1787e2fa1e0df9558c4a27a24
Alex Hung <alex.hung@canonical.com>
2021-06-04 13:47:43 -0600

    acpi: sbbr: add mcfg and pptt to --sbbr option
    Both tables are SBBR mandatory
    
    Signed-off-by: Alex Hung <alex.hung@canonical.com>
    Acked-by: Ivan Hu <ivan.hu@canonical.com>
    Acked-by: Colin Ian King <colin.king@canonical.com>

--- src/acpi/mcfg/mcfg.c
+++ src/acpi/mcfg/mcfg.c
@@ -252,6 +252,7 @@ static fwts_framework_ops mcfg_ops = {
 	.minor_tests = mcfg_tests
 };
 
-FWTS_REGISTER("mcfg", &mcfg_ops, FWTS_TEST_ANYTIME, FWTS_FLAG_BATCH | FWTS_FLAG_ROOT_PRIV | FWTS_FLAG_ACPI)
+FWTS_REGISTER("mcfg", &mcfg_ops, FWTS_TEST_ANYTIME,
+	      FWTS_FLAG_BATCH | FWTS_FLAG_ROOT_PRIV | FWTS_FLAG_ACPI  | FWTS_FLAG_SBBR)
 
 #endif

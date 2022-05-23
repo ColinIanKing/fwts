d82204d836a6e6bf1ce491224923ea5072bed967
Ivan Hu <ivan.hu@canonical.com>
2022-05-23 13:53:49 +0800

    lib: fwts_version.h - update to V22.05.00
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

--- src/lib/include/fwts_version.h
+++ src/lib/include/fwts_version.h
@@ -16,5 +16,5 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  */
-#define FWTS_VERSION "V22.03.00"
-#define FWTS_DATE    "2022-03-17 06:10:44"
+#define FWTS_VERSION "V22.05.00"
+#define FWTS_DATE    "2022-05-23 05:53:49"

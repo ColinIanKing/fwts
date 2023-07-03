ColinIanKing/fwts

commit 06e1d185d2942d1a751bb1edae988eeb409cb29c

fwts_acpica: skip unneccessary null check
Buglink: https://bugs.launchpad.net/bugs/1952671

"context" is always passed as NULL so it should not be used or checked.

Signed-off-by: Alex Hung <alex.hung@canonical.com>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/libfwtsacpica/fwts_acpica.c
@@ -313,9 +313,6 @@ static ACPI_STATUS fwts_region_handler(
 			case AML_FIELD_ATTRIB_BYTES:
 			case AML_FIELD_ATTRIB_RAW_BYTES:
 			case AML_FIELD_ATTRIB_RAW_PROCESS_BYTES:
-				if (!context)
-					return AE_BAD_PARAMETER;
-				length = context->AccessLength - 2;
 				break;
 			default:
 				break;
@@ -340,9 +337,6 @@ static ACPI_STATUS fwts_region_handler(
 			case AML_FIELD_ATTRIB_BYTES:
 			case AML_FIELD_ATTRIB_RAW_BYTES:
 			case AML_FIELD_ATTRIB_RAW_PROCESS_BYTES:
-				if (!context)
-					return AE_BAD_PARAMETER;
-				length = context->AccessLength - 2;
 				break;
 			default:
 				break;

commit 0b728f8a1031e4a968f809362623905bdf0a59e5

fwts_acpica: Remove unused variable "context"
The variable "context" is unused since commit 06e1d185d294
and thus triggers a compiler warning. Remove it to fix this
warning.

Fixes: 06e1d185d294 ("fwts_acpica: skip unneccessary null check")
Signed-off-by: Armin Wolf <W_Armin@gmx.de>
Acked-by: Ivan Hu <ivan.hu@canonical.com>

src/libfwtsacpica/fwts_acpica.c
@@ -262,7 +262,6 @@ static ACPI_STATUS fwts_region_handler(
 	UINT8                   *buffer = ACPI_CAST_PTR(UINT8, value);
 	ACPI_SIZE               length;
 	UINT32                  bytewidth;
-	ACPI_CONNECTION_INFO    *context;
 	int			i;
 
 	if (!regionobject)
@@ -273,8 +272,6 @@ static ACPI_STATUS fwts_region_handler(
 		return AE_OK;
 
 	fwts_acpi_region_handler_called_set(true);
-
-	context = ACPI_CAST_PTR (ACPI_CONNECTION_INFO, handlercontext);
 
 	switch (regionobject->Region.SpaceId) {
 	case ACPI_ADR_SPACE_SYSTEM_IO:
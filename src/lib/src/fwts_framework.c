9e338a17ceab18f7a8be8f6bc8759ef974a4268f
Ivan Hu <ivan.hu@canonical.com>
2023-12-05 14:48:24 +0800

    lib: framework: clear the error message with the extra result filename suffix
    BugLink: https://bugs.launchpad.net/ubuntu/+source/fwts/+bug/2037502
    
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

--- src/lib/src/fwts_framework.c
+++ src/lib/src/fwts_framework.c
@@ -1564,10 +1564,11 @@ int fwts_framework_args(const int argc, char **argv)
 			(fw->flags & FWTS_FLAG_FORCE_CLEAN) ? "w" : "a",
 			fw->log_type)) == NULL) {
 		ret = FWTS_ERROR;
-		fprintf(stderr, "%s: Cannot open results log '%s.log'"
+		fprintf(stderr, "%s: Cannot open results log '%s'"
 			" (you may need to remove it to set proper"
 			" permissions).\n",
-			argv[0], fw->results_logname);
+			argv[0],
+			fwts_log_get_filenames(fw->results_logname, fw->log_type));
 		goto tidy_close;
 	}
 

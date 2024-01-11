a15dca77b4213c28b9d882027846f091c609b28a
Ivan Hu <ivan.hu@canonical.com>
2024-01-11 14:02:28 +0800

    fwts-test: add regression tests for MISC
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

---
+++ fwts-test/misc-0001/test-0001.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+TEST="Test acpitables against MISC"
+NAME=test-0001.sh
+TMPLOG=$TMP/misc.log.$$
+
+$FWTS --show-tests | grep misc > /dev/null
+if [ $? -eq 1 ]; then
+	echo SKIP: $TEST, $NAME
+	exit 77
+fi
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/misc-0001/acpidump-0001.log misc - | cut -c7- | grep "^misc" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/misc-0001/misc-0001.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+rm $TMPLOG
+exit $ret

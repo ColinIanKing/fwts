14b868b3a9528e389e8a6e58afc8028b5784eeee
Ivan Hu <ivan.hu@canonical.com>
2024-01-15 11:31:57 +0800

    fwts-test: add regression tests for SKVL
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

---
+++ fwts-test/skvl-0001/test-0001.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+TEST="Test acpitables against SKVL"
+NAME=test-0001.sh
+TMPLOG=$TMP/skvl.log.$$
+
+$FWTS --show-tests | grep skvl > /dev/null
+if [ $? -eq 1 ]; then
+	echo SKIP: $TEST, $NAME
+	exit 77
+fi
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/skvl-0001/acpidump-0001.log skvl - | cut -c7- | grep "^skvl" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/skvl-0001/skvl-0001.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+rm $TMPLOG
+exit $ret

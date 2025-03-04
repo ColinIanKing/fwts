c78379d4fe5d79b310e6b2a2795987f4e41e4577
Ivan Hu <ivan.hu@canonical.com>
2025-03-04 13:45:28 +0800

    fwts-test: add regression tests for APMT
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

---
+++ fwts-test/apmt-0001/test-0001.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+TEST="Test acpitables against APMT"
+NAME=test-0001.sh
+TMPLOG=$TMP/apmt.log.$$
+
+$FWTS --show-tests | grep apmt > /dev/null
+if [ $? -eq 1 ]; then
+	echo SKIP: $TEST, $NAME
+	exit 77
+fi
+
+$FWTS --log-format="%line %owner " -w 80 --dumpfile=$FWTSTESTDIR/apmt-0001/acpidump-0001.log apmt - | cut -c7- | grep "^apmt" > $TMPLOG
+diff $TMPLOG $FWTSTESTDIR/apmt-0001/apmt-0001.log >> $FAILURE_LOG
+ret=$?
+if [ $ret -eq 0 ]; then
+	echo PASSED: $TEST, $NAME
+else
+	echo FAILED: $TEST, $NAME
+fi
+
+rm $TMPLOG
+exit $ret
